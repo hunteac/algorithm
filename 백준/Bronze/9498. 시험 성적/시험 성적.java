d26f0dec076b8c0eb634d5df234e804614fb6d3a
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-07-19 08:35:07 +0900

    [Bronze V] Title: 시험 성적, Time: 108 ms, Memory: 12844 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		if (90 <= a && a <= 100) {
+			System.out.println("A");
+		} else if (80 <= a && a <= 89) {
+			System.out.println("B");
+		} else if (70 <= a && a <= 79) {
+			System.out.println("C");
+		} else if (60 <= a && a <= 69) {
+			System.out.println("D");
+		} else {
+			System.out.println("F");
+		}
+	}
+}
