20fdaa61db594c7bd802cae652d7d027cf8634d1
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-07-24 22:11:06 +0900

    [Bronze V] Title: A+B - 5, Time: 224 ms, Memory: 17912 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			if (a + b != 0) {
+				System.out.println(a + b);
+			} else {
+				break;
+			}
+		}
+
+		sc.close();
+
+	}
+}
