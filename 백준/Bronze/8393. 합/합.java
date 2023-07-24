049a0fab2a4b4d1e0caf8646c7cfdc1cb1a5f3b0
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-07-24 22:10:00 +0900

    [Bronze V] Title: 합, Time: 216 ms, Memory: 17600 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int sum = 0;
+		
+		for (int i = 1; i <= a; i++) {
+			sum += i;
+		}
+		
+		System.out.println(sum);
+	}
+}
