a4591ce3e7f44d78e08bda60e3e087c7b705970f
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-07-27 11:23:27 +0900

    [Bronze V] Title: 새싹, Time: 80 ms, Memory: 11480 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+    	System.out.printf("         %c%c%c%c%c\n", ',', 'r', '\'', '\"', '7');
+    	System.out.printf("%c%c%c%c   %c%c  %c%c\n", 'r', '`', '-', '_', ',', '\'', ',', '/');
+    	System.out.printf(" %c%c %c%c %c%c%c%c\n", '\\', '.', '\"', '.', 'L', '_', 'r', '\'');
+    	System.out.printf("   %c%c%c%c\n", '`', '~', '\\', '/');
+    	System.out.printf("      %c\n", '|');
+    	System.out.printf("      %c\n", '|');
+   	}
+    
+}
