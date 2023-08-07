d37ac70fb9b7cf1ef18e21d54c13b070b005ee70
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-08-07 13:44:16 +0900

    [D2] Title: Gravity, Time: 139 ms, Memory: 20,684 KB -BaekjoonHub

--- SWEA/D2/16504. Gravity/Gravity.java
+++ SWEA/D2/16504. Gravity/Gravity.java
@@ -13,8 +13,10 @@ class Solution {
 				arr[i] = sc.nextInt();
 			}
 
-			int cnt = 0;
-			int max = 0;
+			int cnt = 0; // 감소하는 순서쌍의 개수를 세는 변수
+			int max = 0; // 최대 감소하는 순서쌍의 개수를 저장하는 변수
+
+			// 모든 i에 대해 i번째 원소와 i+1번째 원소를 비교하여 감소하는 순서쌍의 개수 세기
 			for (int i = 0; i < n - 1; i++) {
 				cnt = 0;
 				for (int j = i + 1; j < n; j++) {
@@ -22,7 +24,7 @@ class Solution {
 						cnt++;
 					}
 					if (cnt > max) {
-						max = cnt;
+						max = cnt; // 최대 감소하는 순서쌍 개수 갱신
 					}
 				}
 			}
