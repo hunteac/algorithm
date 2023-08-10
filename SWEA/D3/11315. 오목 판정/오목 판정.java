94464252e8d628cc2eef4c2c3417399ba7bac032
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-08-10 20:31:49 +0900

    [D3] Title: 오목 판정, Time: 169 ms, Memory: 26,968 KB -BaekjoonHub

--- SWEA/D3/11315. 오목 판정/오목 판정.java
+++ SWEA/D3/11315. 오목 판정/오목 판정.java
@@ -6,100 +6,45 @@ class Solution {
         int T = sc.nextInt();
         for (int test_case = 1; test_case <= T; test_case++) {
             int n = sc.nextInt();
-            String[][] board = new String[n][n];
-
+            char[][] board = new char[n][n];
             for (int i = 0; i < n; i++) {
                 String s = sc.next();
                 for (int j = 0; j < n; j++) {
-                    board[i][j] = s.charAt(j) + "";
+                    board[i][j] = s.charAt(j);
                 }
             }
-            int answer = 0;
-            int cnt = 0;
-            // 행 우선순회
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (board[i][j].equals("o")) {
-                        cnt++;
-                        // 행 체크
-                        for (int k = 1; k < 5; k++) {
-                            if (j + k < n && board[i][j + k].equals("o")) {
-                                cnt++;
-                                if (cnt == 5)
-                                    answer = 1;
-                            } else {
-                            	cnt = 0;
-                            	break;
-                            }
-                        }
-                        // 열 체크
-                        cnt = 1;
-                        for (int k = 1; k < 5; k++) {
-                            if (i + k < n && board[i + k][j].equals("o")) {
-                                cnt++;
-                                if (cnt == 5)
-                                    answer = 1;
-                            } else {
-                            	cnt = 0;
-                            	break;
-                            }
-                        }
-                        // 왼쪽 위, 오른쪽 아래 대각선 확인
-                        cnt = 1;
-                        for (int k = 1; k < 5; k++) {
-                            if (i - k >= 0 && j - k >= 0) {
-                                if (board[i - k][j - k].equals("o")) {
-                                	cnt++;
-                                	if (cnt == 5)
-                                		answer = 1;
-                                } else {
-                                	cnt = 0;
-                                	break;
-                                }
-                            }
-                            if (i + k < n && j + k < n) {
-                                if (board[i + k][j + k].equals("o")) {
-                                	cnt++;
-                                	if (cnt == 5)
-                                		answer = 1;
-                                } else {
-                                	cnt = 0;
-                                	break;
-                                }
-                            }
-                        }
-                        // 오른쪽 위, 왼쪽 아래 대각선 확인
-                        cnt = 1;
-                        for (int k = 1; k < 5; k++) {
-                            if (i - k >= 0 && j + k < n) {
-                                if (board[i - k][j + k].equals("o")) {
-                                	cnt++;
-                                	if (cnt == 5)
-                                		answer = 1;
-                                } else {
-                                	cnt = 0;
-                                	break;
-                                }
-                            }
-                            if (j - k >= 0 && i + k < n) {
-                                if (board[i + k][j - k].equals("o")) {
-                                	cnt++;
-                                	if (cnt == 5)
-                                		answer = 1;
-                                } else {
-                                	cnt = 0;
-                                	break;
-                                }
-                            }
-                        }
-                    }
-                    cnt = 0;
-                }
+            // 8방향 순회를 위한 1차원 배열 두개 생성
+            int[] dr = { -1, -1, 0, 1, 1, 1, 0, -1 };
+            int[] dc = { 0, 1, 1, 1, 0, -1, -1, -1 };
+            boolean flag = false;
+            OuterLoop: for (int i = 0; i < n; i++) {
+            	for (int j = 0; j < n; j++) {
+            		if (board[i][j] == 'o') {
+            			for (int k = 0; k < 8; k++) {
+            				int cnt = 1;
+            				int ti = i;
+            				int tj = j;
+            				while (true) {
+            					int tc = tj + dc[k]; 
+            					int tr = ti + dr[k];
+            					
+            					if (tc >= 0 && tr >= 0 && tc < n && tr < n && board[tr][tc] == 'o') {
+            						ti = tr;
+            						tj = tc;
+            						cnt++;
+            					} else
+            						break;
+            					if (cnt == 5) {
+            						flag = true;
+            						break OuterLoop;
+            					}
+            				}
+            			}
+            		}
+            	}
             }
-            if (answer == 1)
-                System.out.println("#" + test_case + " " + "YES");
-            else
-                System.out.println("#" + test_case + " " + "NO");
+            if (flag) System.out.printf("#%d %s\n", test_case, "YES");
+            else System.out.printf("#%d %s\n", test_case, "NO");
         }
         sc.close();
     }
