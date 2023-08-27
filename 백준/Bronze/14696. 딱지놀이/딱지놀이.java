42216959afbac0b2cb7067c76f43ceccd088932b
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-08-28 00:55:24 +0900

    [Bronze I] Title: 딱지놀이, Time: 240 ms, Memory: 26836 KB -BaekjoonHub

--- 백준/Bronze/14696. 딱지놀이/딱지놀이.java
+++ 백준/Bronze/14696. 딱지놀이/딱지놀이.java
@@ -1,31 +1,39 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt(); // 라운드 수
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		int T = Integer.parseInt(br.readLine()); // 라운드 수
 		for (int tc = 1; tc <= T; tc++) {
 			int[] A = new int[5]; // A가 가지고 있는 딱지
 			int[] B = new int[5]; // B가 가지고 있는 딱지
-			int anum = sc.nextInt(); // A가 가지고 있는 딱지 수
+			st = new StringTokenizer(br.readLine());
+			int anum = Integer.parseInt(st.nextToken());// A가 가지고 있는 딱지 수
 			for (int i = 0; i < anum; i++) {
-				A[sc.nextInt()]++;
+				A[Integer.parseInt(st.nextToken())]++;
 			}
-			int bnum = sc.nextInt(); // B가 가지고 있는 딱지 수	
+			st = new StringTokenizer(br.readLine());
+			int bnum = Integer.parseInt(st.nextToken()); // B가 가지고 있는 딱지 수	
 			for (int i = 0; i < bnum; i++) {
-				B[sc.nextInt()]++;
+				B[Integer.parseInt(st.nextToken())]++;
 			}
 			for (int i = 4; i >= 1; i--) { 
 				if (A[i] > B[i]) { // 딱지 비교
-					System.out.println("A");
+					sb.append("A").append("\n");
 					break;
 				} else if (A[i] < B[i]) {
-					System.out.println("B");
+					sb.append("B").append("\n");
 					break;
 				}
 				if (i == 1 && A[i] == B[i])
-					System.out.println("D");
+					sb.append("D").append("\n");
 			}
 		}
+		System.out.println(sb);
 	}
 }
