0beb8a4c4f1755cb1a9b92129d0ded2f9e64d8b1
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-08-29 16:53:30 +0900

    [D3] Title: [S/W 문제해결 응용] 1일차 - 단순 2진 암호코드, Time: 160 ms, Memory: 21,668 KB -BaekjoonHub

--- SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.java
+++ SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.java
@@ -2,28 +2,30 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
+    public static String[] code = { "1011000", "1001100", "1100100", "1011110", "1100010", "1000110", "1111010", "1101110", "1110110", "1101000" };
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
         for (int tc = 1; tc <= T; tc++) {
             int N = sc.nextInt(); // 배열 세로 크기
             int M = sc.nextInt(); // 배열 가로 크기
-            char[][] code = new char[N][M];
+            char[][] codes = new char[N][M];
             int[] nums = new int[8];
             for (int i = 0; i < N; i++) {
                 String s = sc.next();
                 for (int j = 0; j < M; j++) {
-                    code[i][j] = s.charAt(j); // 암호 넣기
+                    codes[i][j] = s.charAt(j); // 암호 넣기
                 }
             }
             int cnt = 0;
             int cnt2 = 0;
             char[] carr = new char[7];
-            Out: for (int i = N - 1; i >= 0; i--) {
+            Out: for (int i = N - 1; i >= 0; i--) { // 거꾸로 해독
                 for (int j = M - 1; j >= 0; j--) {
-                    if (code[i][j] == '1') { // 거꾸로 해독
+                    if (codes[i][j] == '1') { // 암호문 시작
                         for (int k = 0; k < 56; k++) {
-                            carr[cnt++] = code[i][j - k];
+                            carr[cnt++] = codes[i][j - k];
                             if (cnt == 7) { // 비트가 7개면
                                 String s = new String(carr);
                                 nums[cnt2++] = interpret(s); // 문자열 해독
@@ -49,26 +51,10 @@ public class Solution {
         }
     }
     public static int interpret(String s) {
-        if (s.equals("1011000"))
-            return 0;
-        else if (s.equals("1001100"))
-            return 1;
-        else if (s.equals("1100100"))
-            return 2;
-        else if (s.equals("1011110"))
-            return 3;
-        else if (s.equals("1100010"))
-            return 4;
-        else if (s.equals("1000110"))
-            return 5;
-        else if (s.equals("1111010"))
-            return 6;
-        else if (s.equals("1101110"))
-            return 7;
-        else if (s.equals("1110110"))
-            return 8;
-        else if (s.equals("1101000"))
-            return 9;
+        for (int i = 0; i < 10; i++) {
+            if (s.equals(code[i])) // 문자열이 배열 안의 코드와 동일하다면
+                return i; // 해당 숫자 반환
+        }
         return 0;
     }
 }
