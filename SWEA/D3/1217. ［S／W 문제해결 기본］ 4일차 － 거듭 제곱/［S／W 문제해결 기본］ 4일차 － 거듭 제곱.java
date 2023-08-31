10964d68bebe5db449349dde1169aa699aa7a31f
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-08-31 10:12:59 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 134 ms, Memory: 21,100 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
@@ -1,19 +1,27 @@
 import java.util.Scanner;
 
-class Solution {
-	public static void main(String args[]) {
-		Scanner sc = new Scanner(System.in);
-		for (int test_case = 1; test_case <= 10; test_case++) {
-			int num = sc.nextInt();
-			int n = sc.nextInt(); // 곱하는 수
-			int m = sc.nextInt(); // 거듭제곱 횟수
-			System.out.printf("#%d %d\n", num, multiple(n, m));
-		}
-	}
-	// 재귀호출 활용
-	public static int multiple(int n, int m) {
-		if (m == 0) return 1;
-		m--;
-		return n * multiple(n, m);
-	}
-}	
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for (int tc = 1; tc <= 10; tc++) {
+            int num = sc.nextInt();
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            System.out.println("#" + tc + " " + pow(N, M));
+        }
+    }
+    // 재귀를 이용한 거듭제곱 메소드
+    public static int pow(int N, int M) {
+        int tmp = 0;
+        // 기저파트
+        if (M == 1) return N;
+        // 재귀파트
+        if (M % 2 == 0) { // N이 짝수일 때
+            tmp = pow(N, M / 2);
+            return tmp * tmp;
+        } else { // M이 홀수일 때
+            tmp = pow(N, M / 2);
+            return tmp * tmp * N;
+        }
+    }
+}
