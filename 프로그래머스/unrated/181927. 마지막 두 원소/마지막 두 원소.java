20169dc25e651c54a2ad7bbec80de7a6d342968b
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-09-02 22:34:49 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 71 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length + 1];
+        for (int i = 0; i < num_list.length; i++) {
+            answer[i] = num_list[i];
+            if (i == num_list.length - 1) {
+                if (num_list[i] > num_list[i - 1]) answer[i + 1] = num_list[i] - num_list[i - 1];
+                else answer[i + 1] = num_list[i] * 2;
+            }
+        }
+        return answer;
+    }
+}
