b125519f86377a754eb6bac4c4df3bf4152c9107
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-09-03 22:29:50 +0900

    [unrated] Title: 코드 처리하기, Time: 0.02 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(String code) {
+        String ret = "";
+        int mode = 0;
+        for (int i = 0; i < code.length(); i++) {
+            if (mode == 0) {
+                if (code.charAt(i) == '1') mode = 1;
+                else {
+                    if (i % 2 == 0) ret += code.charAt(i);
+                }
+            } else {
+                if (code.charAt(i) == '1') mode = 0;
+                else {
+                    if (i % 2 == 1) ret += code.charAt(i);
+                }
+            }
+        }
+        if (ret.equals("")) return "EMPTY";
+        else return ret;
+    }
+}
