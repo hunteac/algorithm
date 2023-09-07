358a2fd97124b02877e51eb82365c7f813dea5f4
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-09-07 18:00:39 +0900

    [unrated] Title: 세 개의 구분자, Time: 19.46 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.java
@@ -1,26 +1,22 @@
+import java.util.Arrays;
+
 class Solution {
     public String[] solution(String myStr) {
-        String[] tmp = new String[myStr.length()];
-        String tmps = "";
-        int idx = 0; // 인덱스 변수
-        int cnt = 0; // 문자열 개수 변수
-        for (int i = 0; i < myStr.length(); i++) {
-            tmps = "";
-            while (i + idx < myStr.length() && myStr.charAt(i + idx) != 'a' && myStr.charAt(i + idx) != 'b' && myStr.charAt(i + idx) != 'c') { // 해당 문자가 a, b, c가 아닐때까지 문자 더하기
-               tmps += myStr.charAt(i + idx++);
-            }
-            if (!tmps.equals(""))
-                tmp[cnt++] = tmps; // 구한 문자열 배열에 담기
-            i += idx; // idx만큼 반복문 건너뛰기
-            idx = 0;
+        int cnt = 0;
+        myStr = myStr.replace("a", " "); // a를 공백으로 변환
+        myStr = myStr.replace("b", " "); // b를 공백으로 변환
+        myStr = myStr.replace("c", " "); // c를 공백으로 변환
+        String[] tmp = myStr.split(" "); 
+        for (int i = 0; i < tmp.length; i++) {
+            if (!tmp[i].equals("")) cnt++;
         }
-        if (cnt == 0) cnt = 1; // a, b, c말고 문자가 없는 경우 "EMPTY"를 담기 위해 배열 크기 1로 선언
-        String[] answer = new String[cnt];
+        if (cnt == 0) cnt = 1;
+        String[] answer = new String[cnt]; // 배열 선언
         cnt = 0;
-        for (String s : tmp) {
-            if (s != null) answer[cnt++] = s;
+        for (int i = 0; i < tmp.length; i++) {
+            if (!tmp[i].equals("")) answer[cnt++] = tmp[i]; // 배열에 문자열 담기
         }
-        if (cnt == 0) answer[0] = "EMPTY"; // a, b, c말고 문자가 없는 경우 "EMPTY" 담기
+        if (cnt == 0) answer[0] = "EMPTY"; // 배열이 비었다면 EMPTY 담기
         return answer;
     }
 }
