8a76cb51c72c13306eac90e657506874d9711e1a
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-09-12 17:35:19 +0900

    [level 0] Title: 외계어 사전, Time: 0.05 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.java
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.java
@@ -3,23 +3,19 @@ class Solution {
         boolean[] chk = new boolean[spell.length];
         int answer = 2;
         int cnt = 0;
-        for (int i = 0; i < dic.length; i++) {
+        Outer: for (int i = 0; i < dic.length; i++) {
             cnt = 0;
-            for (int j = 0; j < chk.length; j++) {
-                chk[j] = false;
-            }
+            chk = new boolean[spell.length];
             for (int j = 0; j < spell.length; j++) {
                 for (int k = 0; k < dic[i].length(); k++) {
-                    if (spell[j].equals(String.valueOf(dic[i].charAt(k)))) chk[j] = true;
+                    if (spell[j].equals(String.valueOf(dic[i].charAt(k)))) chk[j] = true; // 해당 알파벳을 사용하면 true
                 }
             }
             for (int j = 0; j < chk.length; j++) { // spell에 담긴 알파벳을 모두 한 번씩 사용했는지 확인
-                if (chk[j]) cnt++;
+                if (!chk[j]) continue Outer; // false가 존재하면 continue
             }
-            if (cnt == spell.length) { // 모두 한 번씩 사용했다면 answer = 1
-                answer = 1;
-                break;
-            }
+            answer = 1; // 모두 한 번씩 사용했다면 answer = 1
+            break;
         }
         return answer;
     }
