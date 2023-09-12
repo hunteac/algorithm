c52d7ba5808e7d196e675301336cd168326383bf
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-09-12 15:18:16 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.03 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -1,32 +1,12 @@
 class Solution {
     public int solution(int[][] lines) {
         int answer = 0;
-        int[] chk = new int[201]; // 두 개 이상 겹치는 선분들의 길이 중복 계산 X
-        for (int i = 0; i < 2; i++) {
-            for (int j = i + 1; j < 3; j++) {
-                if (lines[i][0] <= lines[j][0] && lines[i][1] >= lines[j][1]) {
-                    for (int k = lines[j][0]; k < lines[j][1]; k++) {
-                        chk[100 - k]++;
-                    }
-                } else if (lines[j][0] <= lines[i][0] && lines[j][1] >= lines[i][1]) {
-                    for (int k = lines[i][0]; k < lines[i][1]; k++) {
-                        chk[100 - k]++;
-                    }
-                } else if (lines[i][1] >= lines[j][1]) {
-                    if (lines[j][1] < lines[i][0]) continue;
-                    for (int k = lines[i][0]; k < lines[j][1]; k++) {
-                        chk[100 - k]++;
-                    }
-                 } else if (lines[j][1] > lines[i][1]) {
-                    if (lines[j][0] > lines[i][1]) continue;
-                    for (int k = lines[j][0]; k < lines[i][1]; k++) {
-                        chk[100 - k]++;
-                    }
-                }    
-            }
-        }
-        for (int i = 0; i < chk.length; i++) {
-            if (chk[i] > 0) answer++;
+        for (int i = -100; i < 100; i++) {
+            int line = 0;
+            if (lines[0][0] <= i && lines[0][1] > i) line++;
+            if (lines[1][0] <= i && lines[1][1] > i) line++;
+            if (lines[2][0] <= i && lines[2][1] > i) line++;
+            if (line > 1) answer++; // 겹치는 선분 길이 더하기
         }
         return answer;
     }
