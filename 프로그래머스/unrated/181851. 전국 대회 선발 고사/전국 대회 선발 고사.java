b444badbe38fb096036fc06825f2666084db8fbb
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-09-12 17:14:35 +0900

    [unrated] Title: 전국 대회 선발 고사, Time: 0.43 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -1,34 +1,28 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] rank, boolean[] attendance) {
-        int min = 101;
+        int[] tmp = new int[rank.length];
         int first = 0; // 1등 순서
         int second = 0; // 2등 순서
         int third = 0; // 3등 순서
-        for (int i = 0; i < rank.length; i++) { // 1등 순서 구하기
-            if (!attendance[i]) continue;
-            if (rank[i] < min) {
-                min = rank[i];
-                first = i;
-            }
+        for (int i = 0; i < rank.length; i++) {
+            tmp[i] = rank[i];
+            if (!attendance[i]) tmp[i] = 101;
         }
-        min = 101;
-        for (int i = 0; i < rank.length; i++) { // 2등 순서 구하기
-            if (!attendance[i]) continue;
-            if (rank[i] < min) {
-                if (rank[i] > rank[first]) {
-                    min = rank[i];
-                    second = i;
-                }
-            }
-        }
-        min = 101;
-        for (int i = 0; i < rank.length; i++) { // 3등 순서 구하기
-            if (!attendance[i]) continue;
-            if (rank[i] < min) {
-                if (rank[i] > rank[first]) {
-                    if (rank[i] > rank[second]) {
-                        min = rank[i];
-                        third = i;    
+        Arrays.sort(tmp);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < rank.length; j++) {
+                if (tmp[i] == rank[j]) {
+                    if (i == 0) { // 1등 순서 구하기
+                        first = j;
+                        break;
+                    } else if (i == 1) { // 2등 순서 구하기
+                        second = j;
+                        break;
+                    } else { // 3등 순서 구하기
+                        third = j;
+                        break;
                     }
                 }
             }
