46d934572b079756e0a244d259342579011a8a54
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-09-13 15:49:22 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(long num) {
+        int answer = 0; // 작업 횟수
+        while (num != 1) {
+            if (num % 2 == 0) num /= 2;
+            else num = (num * 3) + 1;
+            answer++;
+            if (answer == 500) {
+                answer = -1;
+                break;
+            }
+        }
+        return answer;
+    }
+}
