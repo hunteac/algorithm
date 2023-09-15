abec0d15a7b8f7dfc286df450ff7b819d822517f
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-09-15 23:47:43 +0900

    [D1] Title: 신문 헤드라인, Time: 127 ms, Memory: 19,900 KB -BaekjoonHub

---
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.next().toUpperCase()); // 대문자로 변환
+    }
+}
