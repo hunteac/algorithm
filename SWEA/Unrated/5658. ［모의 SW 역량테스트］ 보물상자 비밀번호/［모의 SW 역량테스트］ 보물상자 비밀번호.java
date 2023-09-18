f6b37328e4c491edc797f4033b990b93036855ea
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-09-18 21:56:40 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보물상자 비밀번호, Time: 225 ms, Memory: 28,112 KB -BaekjoonHub

--- SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.java
+++ SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.java
@@ -6,39 +6,31 @@ public class Solution {
         int T = sc.nextInt();
         for (int tc = 1; tc <= T; tc++) {
             StringBuilder sb = new StringBuilder();
-            List<String> sPwds = new ArrayList<>(); // 문자열 비밀번호
-            List<Integer> iPwds = new ArrayList<>(); // 정수 비밀번호
+            List<Integer> Pwd = new ArrayList<>(); // 비밀번호
             int N = sc.nextInt();
             int K = sc.nextInt();
             String tBox = sc.next();
             int num = 0;
-            int cnt = 0;
-            for (int i = 0; i < N / 4; i++) { // 모든 비밀번호 구하기 N / 4번 회전
+            for (int i = 0; i < N / 4; i++) { // 모든 비밀번호 구하기 -> N / 4번 회전
                 for (int j = i; j < N + i; j++) {
                     if (j < N) sb.append(tBox.charAt(j));
                     else sb.append(tBox.charAt(j % N));
                     if (sb.length() == N / 4) {
-                        if (!sPwds.contains(sb.toString())) sPwds.add(sb.toString()); // 중복 비밀번호 제거
+                        num = 0;
+                        for (int k = sb.length() - 1; k >= 0; k--) {
+                            if (Character.isDigit(sb.charAt(k))) {
+                                num += (sb.charAt(k) - '0') * (int) Math.pow(16, sb.length() - k - 1);
+                            } else {
+                                num += (sb.charAt(k) - 55) * (int) Math.pow(16, sb.length() - k - 1);
+                            }
+                        }
                         sb.setLength(0); // sb 초기화
+                        if (!Pwd.contains(num)) Pwd.add(num);
                     }
                 }
             }
-            for (int i = 0; i < sPwds.size(); i++) { // 16진법 -> 10진법 변환
-                String s = sPwds.get(i);
-                num = 0;
-                cnt = 0;
-                for (int j = s.length() - 1; j >= 0; j--) {
-                    if (Character.isDigit(s.charAt(j))) {
-                        num += (s.charAt(j) - '0') * (int) Math.pow(16, cnt);
-                    } else {
-                        num += (s.charAt(j) - 55) * (int) Math.pow(16, cnt);
-                    }
-                    cnt++;
-                }
-                iPwds.add(num);
-            }
-            Collections.sort(iPwds, (o1, o2) -> (o2 - o1)); // 내림차순 정렬
-            System.out.println("#" + tc + " " + iPwds.get(K - 1)); // K번째로 큰 수 출력
+            Collections.sort(Pwd, (o1, o2) -> (o2 - o1)); // 내림차순 정렬
+            System.out.println("#" + tc + " " + Pwd.get(K - 1)); // K번째로 큰 수 출력
         }
     }
 }
