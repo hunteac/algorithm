9396964976dc9bc42b961b91bde4a08377cee053
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-09-18 17:09:41 +0900

    [D3] Title: 그래프의 삼각형, Time: 381 ms, Memory: 93,224 KB -BaekjoonHub

--- SWEA/D3/6057. 그래프의 삼각형/그래프의 삼각형.java
+++ SWEA/D3/6057. 그래프의 삼각형/그래프의 삼각형.java
@@ -1,29 +1,43 @@
 import java.util.Scanner;
 
 public class Solution {
+    public static int N; // 정점 개수
+    public static int M; // 간선 개수
+    public static int answer; // 삼각형 개수
+    public static int[][] adjArr; // 인접 행렬
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
         for (int tc = 1; tc <= T; tc++) {
-            int N = sc.nextInt(); // 정점 개수
-            int M = sc.nextInt(); // 간선 개수
-            int answer = 0; // 삼각형 개수
-            int[][] adjArr = new int[N + 1][N + 1]; // 인접 행렬
+            N = sc.nextInt();
+            M = sc.nextInt();
+            adjArr = new int[N + 1][N + 1];
+            answer = 0;
             for (int i = 0; i < M; i++) {
                 int A = sc.nextInt(); // 시작 정점
                 int B = sc.nextInt(); // 끝 정점
                 adjArr[A][B] = 1;
+                adjArr[B][A] = 1;
             }
-            for (int i = 1; i < N - 1; i++) { // 조합 구하는 방식으로 삼각형 개수 구하기
-                for (int j = i + 1; j < N; j++) {
-                    for (int k = j + 1; k < N + 1; k++) {
-                        if (adjArr[i][j] == 1 && adjArr[i][k] == 1 && adjArr[j][k] == 1) {
-                            answer++;
-                        }
-                    }
-                }
-            }
+            getTriangle(0, 0, 0);
             System.out.println("#" + tc + " " + answer);
+        }
+    }
+    // DFS로 삼각형 개수 구하는 메소드
+    public static void getTriangle(int startNode, int node, int len) {
+        if (len == 3) {
+            if (adjArr[node][startNode] == 1) {
+                answer++;
+                return;
+            } else return;
+        }
+        for (int i = node + 1; i < N + 1; i++) {
+            if (startNode == 0) {
+                getTriangle(i, i, len + 1);
+            } else  if (adjArr[i][node] == 1) {
+                getTriangle(startNode, i, len + 1);
+            }
         }
     }
 }
