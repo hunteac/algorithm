4872f81a0d207c9d5e2516dbd0d61dede48b9138
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-09-20 00:42:56 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 236 ms, Memory: 18220 KB -BaekjoonHub

---
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int cnt; // 함수 호출 횟수
+    public static int recursion(String s, int l, int r) {
+        cnt++;
+        if (l >= r) return 1;
+        else if (s.charAt(l) != s.charAt(r)) return 0;
+        else return recursion(s,l + 1, r - 1);
+    }
+    public static int isPalindrome(String s) {
+        return recursion(s, 0, s.length() - 1);
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            cnt = 0;
+            String s = br.readLine();
+            System.out.println(isPalindrome(s) + " " + cnt);
+        }
+    }
+}
