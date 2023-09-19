3fde856c26344b5ae378611147fc53c8abb6ecbb
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-09-19 15:30:34 +0900

    [Silver I] Title: 단지번호붙이기, Time: 92 ms, Memory: 11596 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -5,20 +5,19 @@ import java.util.*;
 
 public class Main {
     public static boolean[][] map; // 지도
-    public static boolean[][] visited; // 방문 확인 배열
     public static int N; // 지도 크기
     public static int cnt = 0; // 단지별 아파트 수
 
-    // BFS (너비 우선 탐색) 메소드
-    public static void BFS(int i, int j) {
+    // DFS (깊이 우선 탐색) 메소드
+    public static void DFS(int i, int j) {
         if (i >= 0 && j >= 0 && i < N && j < N) { // 지도 범위 내에서만 확인
-            if (!map[i][j] || visited[i][j]) return; // 집이 없거나 이미 방문한 곳이면 return
+            if (!map[i][j]) return; // 집이 없으면 return
             cnt++;
-            visited[i][j] = true; // 방문 확인
-            BFS(i - 1, j); // 상
-            BFS(i, j + 1); // 우
-            BFS(i + 1, j); // 하
-            BFS(i, j - 1); // 좌 확인
+            map[i][j] = false; // 방문 한 곳 지우기
+            DFS(i - 1, j); // 상
+            DFS(i, j + 1); // 우
+            DFS(i + 1, j); // 하
+            DFS(i, j - 1); // 좌 확인
         } else return;
     }
 
@@ -27,7 +26,6 @@ public class Main {
         List<Integer> list = new ArrayList<>();
         N = Integer.parseInt(br.readLine());
         map = new boolean[N][N];
-        visited = new boolean[N][N];
         int num = 0; // 단지 수
         for (int i = 0; i < N; i++) {
             String s = br.readLine();
@@ -37,8 +35,8 @@ public class Main {
         }
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (map[i][j] && !visited[i][j]) { // 집이 있고 방문하지 않은 곳만 확인
-                    BFS(i, j);
+                if (map[i][j]) { // 집이 있는 곳 방문
+                    DFS(i, j);
                     list.add(cnt);
                     num++;
                 }
