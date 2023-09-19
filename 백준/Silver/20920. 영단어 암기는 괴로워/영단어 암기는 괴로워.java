3cfcbe1eb41165f5276276aa103847d2e96a9597
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-09-20 00:16:31 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 720 ms, Memory: 38812 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
@@ -1,10 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -14,28 +11,32 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         HashMap<String, Integer> hMap = new HashMap<>(); // 단어 등장 횟수 측정 맵
-        HashSet<String> chk = new HashSet<>(); // 중복 제거 셋
-        String[] words = new String[N];
-        int max = 0;
+        List<String> words = new ArrayList<>(); // 단어장
         for (int i = 0; i < N; i++) {
             String s = br.readLine();
-            if (s.length() >= M) words[i] = s;
-            else words[i] = "";
+            if (s.length() < M) continue;
+            hMap.put(s, hMap.getOrDefault(s, 0) + 1);
         }
-        Arrays.sort(words); // 사전순 정렬
-        Arrays.sort(words, ((o1, o2) -> (o2.length() - o1.length()))); // 길이순 역정렬
-        for (int i = 0; i < N; i++) {
-            if (words[i].isEmpty()) continue;
-            hMap.put(words[i], hMap.getOrDefault(words[i], 0) + 1);
-            max = Math.max(max, hMap.get(words[i])); // 최대 단어 등장 횟수
+        for (String s : hMap.keySet()) {
+            words.add(s);
         }
-        for (int i = max; i >= 1; i--) {
-            if (!hMap.containsValue(i)) continue;
-            for (int j = 0; j < words.length; j++) {
-                if (words[j].isEmpty()) continue;
-                if (hMap.get(words[j]) == i)
-                    if (chk.add(words[j])) sb.append(words[j]).append("\n");
+        Collections.sort(words, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                // 등장 횟수 정렬
+                if (Integer.compare(hMap.get(o1), hMap.get(o2)) != 0) {
+                    return Integer.compare(hMap.get(o2), hMap.get(o1));
+                }
+                // 길이 순 역정렬
+                if (o1.length() != o2.length()) {
+                    return o2.length() - o1.length();
+                }
+                // 등장 횟수와 길이가 같으면 사전 순 정렬
+                return o1.compareTo(o2);
             }
+        });
+        for (String s : words) {
+            sb.append(s).append("\n");
         }
         System.out.println(sb);
     }
