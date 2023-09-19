9478b1abe1ff659435c028f9e3c96876ed25c0fb
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-09-19 12:21:33 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 1172 ms, Memory: 98740 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static ArrayList<Integer>[] graph; // 인접 리스트
+    public static boolean[] visited; // 방문 확인
+    public static int[] order; // 방문 순서 배열
+    public static int cnt = 1; // 방문 순서
+    public static int N; // 정점 수
+    public static int M; // 간선 수
+
+    public static void DFS(int v) {
+        visited[v] = true; // 방문 확인
+        order[v] = cnt++; // 방문 순서 담기
+        for (int nextNode : graph[v]) {
+            if (visited[nextNode]) continue;
+            DFS(nextNode);
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken()); // 시작 정점
+        graph = new ArrayList[N + 1];
+        visited = new boolean[N + 1];
+        order = new int[N + 1];
+        for (int i = 1; i < N + 1; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < M; i++) { // 인접 정보 입력
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            graph[A].add(B);
+            graph[B].add(A);
+        }
+        for (int i = 1; i < N + 1; i++) { // 내림차순 정렬
+            Collections.sort(graph[i], (o1, o2) -> (o2 - o1));
+        }
+        DFS(R);
+        for (int i = 1; i < N + 1; i++) {
+            sb.append(order[i]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
