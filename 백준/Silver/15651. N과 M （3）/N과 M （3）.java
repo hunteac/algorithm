f7badc809e18a5b366afb3c5a95e51f82aedc6d5
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-09-21 00:04:35 +0900

    [Silver III] Title: N과 M (3), Time: 456 ms, Memory: 114640 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+    static int[] nums;
+    static boolean[] visited; // 방문 체크
+    static int N;
+    static int M;
+
+    static void bTrack(int idx) {
+        // 기저
+        if (idx == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(nums[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            nums[idx] = i;
+            bTrack(idx + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        nums = new int[N + 1];
+        visited = new boolean[N + 1];
+        bTrack(0);
+        System.out.println(sb);
+    }
+}
