f07ef3c49f7bff732491b9105cfd383f30269f12
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-09-24 21:37:03 +0900

    [level 2] Title: 타겟 넘버, Time: 1.24 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    static boolean[] visited; // 방문 체크
+    static int answer; // 경우의 수
+    static int sum;
+    
+    // 재귀 함수
+    static void recursion(int[] arr, int target, int idx){
+        sum = 0;
+        if (idx == arr.length) {
+            for (int i = 0; i < arr.length; i++) {
+                if (visited[i]) sum += arr[i];
+                else sum -= arr[i];
+            }
+            if (sum == target) answer++;
+            return ;
+        }
+        
+        visited[idx] = true; // 원소 선택
+        recursion(arr, target, idx + 1);
+        visited[idx] = false; // 원소 선택 X
+        recursion(arr, target, idx + 1);
+    }
+    
+    public int solution(int[] numbers, int target) {
+        visited = new boolean[numbers.length];
+        answer = 0;
+        
+        recursion(numbers, target, 0); // 재귀 함수 호출       
+                                                 
+        return answer;
+    }
+}
