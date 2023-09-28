85dfc80bf986fe9a4b46b793eefdbc7232173322
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-09-28 14:03:02 +0900

    [D1] Title: 아주 간단한 계산기, Time: 128 ms, Memory: 20,772 KB -BaekjoonHub

---
+++ SWEA/D1/1938. 아주 간단한 계산기/아주 간단한 계산기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.printf("%d\n%d\n%d\n%d", a + b, a - b, a * b, a / b);
+    }
+}
