35f0d51d2ceecb98f0356eb386ef4e546fb89527
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-10-15 00:32:03 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 436 ms, Memory: 11540 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
@@ -1,23 +1,28 @@
-import java.util.*;
+import java.io.*;
 
 public class Main {
+    static int recur; // 재귀 함수 실행 횟수
     static int dp; // 동적 프로그래밍 실행 횟수
     static int[] f;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
 
         f = new int[N + 1];
 
+        fibo(N);
         fibonacci(N);
 
-        System.out.println(fibo(N) + " " + dp);
+        System.out.println(recur + " " + dp);
     }
     // 재귀 함수
     static long fibo (int n) {
-        if (n == 1 || n == 2) return 1;
+        if (n == 1 || n == 2) {
+            recur++; // 횟수 증가
+            return 1;
+        }
         else return (fibo(n - 1) + fibo(n - 2));
     }
     // 동적 프로그래밍
@@ -25,7 +30,7 @@ public class Main {
         f[1] = f[2] = 1;
 
         for (int i = 3; i <= n; i++) {
-            dp++;
+            dp++; // 횟수 증가
             f[i] = f[i - 1] + f[i - 2];
         }
 
