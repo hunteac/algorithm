f34e86c23f1e30d43377f7137f576428b48ddea9
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-10-15 21:54:41 +0900

    [Silver III] Title: 파도반 수열, Time: 76 ms, Memory: 11428 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,34 +1,34 @@
-import java.util.*;
+import java.io.*;
 
 public class Main {
     static long[] P; // 파도반 수열
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        int T = sc.nextInt();
+        int T = Integer.parseInt(br.readLine());
 
         P = new long[101];
 
         P[1] = P[2] = P[3] = 1;
         P[4] = P[5] = 2;
-        P[6] = 3;
-        P[7] = 4;
-        P[8] = 5;
 
         for (int tc = 1; tc <= T; tc++) {
-            int N = sc.nextInt();
+            int N = Integer.parseInt(br.readLine());
 
             if (P[N] != 0) {
-                System.out.println(P[N]);
+                sb.append(P[N]).append("\n");
                 continue;
             }
 
-            for (int i = 9; i <= N; i++) {
+            for (int i = 6; i <= N; i++) {
                 P[i] = P[i - 5] + P[i - 1];
             }
 
-            System.out.println(P[N]);
+            sb.append(P[N]).append("\n");
         }
+
+        System.out.println(sb);
     }
 }
