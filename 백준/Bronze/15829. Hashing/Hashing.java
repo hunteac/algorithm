9a451088315621220ea41c1c96272e35982b0097
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-10-26 14:36:14 +0900

    [Bronze II] Title: Hashing, Time: 88 ms, Memory: 11832 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
@@ -9,12 +10,21 @@ public class Main {
 		int N = Integer.parseInt(br.readLine());
 		String s = br.readLine();
 		
-		int hash = 0;
+		BigInteger hash = new BigInteger("0");
+		BigInteger b = new BigInteger("1234567891");
 		
 		for (int i = 0; i < N; i++) {
-			hash += (s.charAt(i) - 96) * Math.pow(31, i);
+			String str = String.valueOf(s.charAt(i) - 96);
+			BigInteger num = new BigInteger(str);
+			BigInteger m = new BigInteger("31");
+					
+			for (int j = 0; j < i; j++) {
+				num = num.multiply(m);
+			}
+			
+			hash = hash.add(num);
 		}
 		
-		System.out.println(hash);
+		System.out.println(hash.mod(b));
 	}
 }
