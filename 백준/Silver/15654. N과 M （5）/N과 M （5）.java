95f27a6cc3b20c9994f282fa0b8d5285db4ce3b2
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-11-02 22:17:22 +0900

    [Silver III] Title: N과 M (5), Time: 244 ms, Memory: 30704 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static boolean[] visited; // 방문 체크
+    static int[] nums;
+    static int[] p;
+    static int N;
+    static int M;
+
+    // 순열 알고리즘
+    static void permutation(int idx) {
+        if (idx == M) {
+            for (int i = 0; i < M ;i++) {
+                sb.append(p[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (visited[i]) continue;
+            visited[i] = true; // 원소 선택
+            p[idx] = nums[i];
+            permutation(idx + 1); // 재귀 함수 호출
+            visited[i] = false; // 원소 선택 X
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[N];
+        nums = new int[N];
+        p = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums); // 오름차순 정렬
+
+        permutation(0);
+
+        System.out.println(sb);
+    }
+}
