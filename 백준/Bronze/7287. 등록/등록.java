472a618cea2cc6098aa8b1d60f8cd471e5938d12
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-11-06 15:04:45 +0900

    [Bronze V] Title: 등록, Time: 72 ms, Memory: 11268 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println(257);
+		System.out.println("98cline");
+	}
+}
