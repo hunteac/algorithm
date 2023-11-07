0fd2170d90ee8779b0f512714b39784797569aab
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-11-07 17:25:57 +0900

    [Silver I] Title: 숫자 재배치, Time: 476 ms, Memory: 208804 KB -BaekjoonHub

--- 백준/Silver/16943. 숫자 재배치/숫자 재배치.java
+++ 백준/Silver/16943. 숫자 재배치/숫자 재배치.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -9,28 +11,31 @@ import java.util.StringTokenizer;
 
 public class Main {
 	static boolean[] visited; // 방문 체크
+	static String[] arr; // A 배열
 	static String[] p; // 순열
 	static String A;
-	static int B;
+	static String B;
 	static int max; // 최댓값
 	static boolean chk; // 가능 여부
 	
 	// 순열 알고리즘
 	static void permutation(int idx, int len) {
+		if (chk) return;
 		if (idx == len) {
 			if (p[0].equals("0")) return;
 			String tmp = "";
 			for (String s : p) tmp += s;
-			if (Integer.parseInt(tmp) < B) {
+			if (Integer.parseInt(tmp) < Integer.parseInt(B)) {
 				chk = true;
-				max = Math.max(max, Integer.parseInt(tmp)); // B보다 작은 수 중 최댓값 구하기
+				max = Integer.parseInt(tmp); // B보다 작은 수 중 최댓값
 			}
+			return;
 		}
 		
 		for (int i = 0; i < len; i++) {
 			if (visited[i]) continue;
 			visited[i] = true; // 원소 선택
-			p[idx] = String.valueOf(A.charAt(i));
+			p[idx] = arr[i];
 			permutation(idx + 1, len);
 			visited[i] = false; // 원소 선택 X
 		}
@@ -41,10 +46,24 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		A = st.nextToken();		
-		B = Integer.parseInt(st.nextToken());		
+		B = st.nextToken();	
 		
+		if (A.length() > B.length()) {
+			System.out.println(-1);
+			return;
+		}
+
+		arr = A.split("");
 		visited = new boolean[A.length()];
 		p = new String[A.length()];
+		
+		// 내림차순 정렬
+		Arrays.sort(arr, new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				return o2.charAt(0) - o1.charAt(0);
+			}
+		});
 		
 		chk = false;
 		
