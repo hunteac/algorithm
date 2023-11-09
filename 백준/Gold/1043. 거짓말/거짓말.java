740a23e573fed2b445aa5453bbc3f8ffdf01a14a
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-11-09 18:04:39 +0900

    [Gold IV] Title: 거짓말, Time: 88 ms, Memory: 11740 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -12,26 +12,26 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken()); // 사람의 수
+		int M = Integer.parseInt(st.nextToken()); // 파티 수
 		
 		st = new StringTokenizer(br.readLine());
 		
-		int T = Integer.parseInt(st.nextToken());
+		int T = Integer.parseInt(st.nextToken()); // 진실을 아는 사람 수
 		
-		if (T == 0) {
-			System.out.println(M);
-			return;
-		} else {
+		if (T == 0) { // 진실을 아는 사람이 없을 때
+			System.out.println(M); // 파티 수 출력
+			return; // 종료
+		} else { // 진실을 아는 사람이 있을 때
 			Set<Integer> truth = new HashSet<>(); // 진실을 아는 사람들
 			List<Integer>[] parties = new ArrayList[M]; // 파티에 참여하는 사람들
 			
 			for (int i = 0; i < T; i++) truth.add(Integer.parseInt(st.nextToken()));
 			for (int i = 0; i < M; i++) parties[i] = new ArrayList<>();
 			
-			int cnt = 0;
+			int cnt = 0; // 과장할 수 있는 파티 수
 			
-			for (int i = 0; i < M; i++) {
+			for (int i = 0; i < M; i++) { // 파티 참여하는 사람들 담기
 				st = new StringTokenizer(br.readLine());
 					
 				int len = Integer.parseInt(st.nextToken());
@@ -47,7 +47,7 @@ public class Main {
 				}
 			}
 			
-			for (int t = 0; t < M; t++) {
+			for (int t = 0; t < M; t++) { // 모든 파티를 돌면서 진실을 아는 사람 모두 구하기
 				for (int i = 0; i < M; i++) {
 					boolean chk = true;
 					for (int j = 0; j < parties[i].size(); j++) {
@@ -64,7 +64,7 @@ public class Main {
 				}
 			}
 			
-			for (int i = 0; i < M; i++) {
+			for (int i = 0; i < M; i++) { // 과장할 수 있는 파티 수 구하기
 				boolean chk = true;
 				for (int j = 0; j < parties[i].size(); j++) {
 					if (truth.contains(parties[i].get(j))) {
@@ -75,7 +75,7 @@ public class Main {
 				if (chk) cnt++;
 			}
 			
-			System.out.println(cnt);
+			System.out.println(cnt); // 출력
 		}
 	}
 }
