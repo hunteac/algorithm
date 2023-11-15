192f7a284753923963cd281bd9d1b6d8e37db993
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-11-15 13:03:21 +0900

    [Silver II] Title: 나무 자르기, Time: 500 ms, Memory: 167856 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken()); // 나무의 수
+		int M = Integer.parseInt(st.nextToken()); // 나무 길이
+		
+		int[] tree = new int[N]; // 나무 배열
+		
+		st = new StringTokenizer(br.readLine());
+		int max = 0; // 최대 나무 높이
+		
+		for (int i = 0; i < N; i++) {
+			tree[i] = Integer.parseInt(st.nextToken());
+			max = Math.max(max, tree[i]); // 최대 나무 높이 구하기
+		}
+		
+		int start = 0; // 시작
+		int end = max; // 끝
+		int mid = 0; // 중앙
+		long tmpsum = 0;
+		long sum = 0;
+		
+		// 이분탐색
+		while (start <= end) {
+			mid = (start + end) / 2;
+			sum = 0;
+			tmpsum = 0;
+			
+			for (int i = 0; i < N; i++) {
+				if (tree[i] <= mid) continue;
+				
+				sum += tree[i] - mid;
+				tmpsum += tree[i] - mid - 1;
+			}
+			
+			if (sum >= M && tmpsum < M) break;
+			
+//			System.out.println("start : " + start + " end : " + end);
+//			System.out.println("mid : " + mid + " sum : " + sum + " tmpsum : " + tmpsum);
+			
+			if (sum < M) end = mid - 1; // 길이가 모자라면 높이 줄이기
+			else if (sum > M) start = mid + 1; // 길이가 남으면 높이 늘리기
+			else break; // 길이가 맞으면 종료
+		}
+		
+		System.out.println(mid);
+	}
+}
