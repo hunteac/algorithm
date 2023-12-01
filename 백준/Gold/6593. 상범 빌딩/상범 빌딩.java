ed2c1e23e61594c8f8024c7ed943d0d671c4ff6e
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-12-01 14:35:29 +0900

    [Gold V] Title: 상범 빌딩, Time: 128 ms, Memory: 15228 KB -BaekjoonHub

--- 백준/Gold/6593. 상범 빌딩/상범 빌딩.java
+++ 백준/Gold/6593. 상범 빌딩/상범 빌딩.java
@@ -7,30 +7,30 @@ import java.util.StringTokenizer;
 
 public class Main {
     static class Pos {
-        int l;
-        int r;
-        int c;
+        int l; // 현재 층
+        int r; // 현재 행
+        int c; // 현재 열
         Pos (int l, int r, int c) {
             this.l = l;
             this.r = r;
             this.c = c;
         }
     }
-    static Queue<Pos> queue;
-    static boolean[][][] visited;
-    static char[][][] building;
-    static int[] dl = { -1, 1, 0, 0, 0, 0 };
-    static int[] dr = { 0, 0, -1, 0, 1, 0 };
-    static int[] dc = { 0, 0, 0, 1, 0, -1 };
-    static int L;
-    static int R;
-    static int C;
-    static int time;
-    static boolean chk;
-
+    static Queue<Pos> queue; // bfs 큐
+    static boolean[][][] visited; // 방문체크
+    static char[][][] building; // 빌딩
+    static int[] dl = { -1, 1, 0, 0, 0, 0 }; // 층 탐색
+    static int[] dr = { 0, 0, -1, 0, 1, 0 }; // 행 탐색
+    static int[] dc = { 0, 0, 0, 1, 0, -1 }; // 열 탐색
+    static int L; // 층
+    static int R; // 행
+    static int C; // 열
+    static int time; // 탈출 시간
+    static boolean chk; // 탈출 가능 여부
+    // 너비 우선 탐색
     static void bfs(int l, int r, int c) {
-        queue.add(new Pos(l, r, c));
-        visited[l][r][c] = true;
+        queue.add(new Pos(l, r, c)); // Start 지점
+        visited[l][r][c] = true; // 방문 처리
 
         Outer: while(!queue.isEmpty()) {
             int len = queue.size();
@@ -38,26 +38,26 @@ public class Main {
             for (int i = 0; i < len; i++) {
                 Pos p = queue.poll();
 
-                l = p.l;
-                r = p.r;
-                c = p.c;
+                l = p.l; // 현재 층
+                r = p.r; // 현재 행
+                c = p.c; // 현재 열
 
-                if (building[l][r][c] == 'E') {
-                    chk = true;
+                if (building[l][r][c] == 'E') { // 탈출지점 도착
+                    chk = true; //
                     break Outer;
                 }
 
                 for (int d = 0; d < 6; d++) {
-                    int nl = l + dl[d];
-                    int nr = r + dr[d];
-                    int nc = c + dc[d];
+                    int nl = l + dl[d]; // 다음 층
+                    int nr = r + dr[d]; // 다음 행
+                    int nc = c + dc[d]; // 다음 열
 
                     if (nl < 0 || nr < 0 || nc < 0 || nl >= L || nr >= R || nc >= C || visited[nl][nr][nc] ||
-                    building[nl][nr][nc] == '#') continue;
+                    building[nl][nr][nc] == '#') continue; // 범위를 벗어나거나 방문했거나 갈 수 없으면 continue
 
-                    visited[nl][nr][nc] = true;
+                    visited[nl][nr][nc] = true; // 방문 처리
 
-                    queue.add(new Pos(nl, nr, nc));
+                    queue.add(new Pos(nl, nr, nc)); // 다음 위치 담기
                 }
             }
 
@@ -107,8 +107,8 @@ public class Main {
 
             bfs(l, r, c);
 
-            if (chk) sb.append("Escaped in " + time + " minute(s).").append("\n");
-            else sb.append("Trapped!").append("\n");
+            if (chk) sb.append("Escaped in " + time + " minute(s).").append("\n"); // 탈출 가능한 경우
+            else sb.append("Trapped!").append("\n"); // 탈출 불가능한 경우
         }
 
         System.out.println(sb);
