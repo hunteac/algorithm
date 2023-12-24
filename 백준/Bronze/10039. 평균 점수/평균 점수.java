70d77c5af91e3a20d789430635f7dbce1d4ec0a6
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-12-24 22:00:01 +1500

    [Bronze IV] Title: 평균 점수, Time: 72 ms, Memory: 11360 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = 0;
+
+        for (int i = 0; i < 5; i++) {
+            int score = Integer.parseInt(br.readLine());
+            if (score < 40) sum += 40;
+            else sum += score;
+        }
+
+        System.out.println(sum / 5);
+    }
+}
