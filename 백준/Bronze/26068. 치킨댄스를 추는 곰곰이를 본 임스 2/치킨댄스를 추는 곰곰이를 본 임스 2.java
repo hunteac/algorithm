33207f57fdc3e0284aa95d694fb42def8281cd15
NewCoding <135293451+HunTeac@users.noreply.github.com>
2023-12-31 22:00:01 +0900

    [Bronze IV] Title: 치킨댄스를 추는 곰곰이를 본 임스 2, Time: 80 ms, Memory: 11772 KB -BaekjoonHub

---
+++ 백준/Bronze/26068. 치킨댄스를 추는 곰곰이를 본 임스 2/치킨댄스를 추는 곰곰이를 본 임스 2.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int cnt = 0;
+
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+
+            if (Integer.parseInt(str.substring(2, str.length())) <= 90) cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+}
