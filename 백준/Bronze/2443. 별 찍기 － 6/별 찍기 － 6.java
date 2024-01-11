3777890c8f89dca8822ff62236023e2199fccb53
NewCoding <135293451+HunTeac@users.noreply.github.com>
2024-01-11 22:00:01 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 84 ms, Memory: 11820 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < 2 * N - 1; i++) {
+            for (int j = 0; j < 2 * N - i - 1; j++) {
+                if (j < i) sb.append(" ");
+                else sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
