8825fea2e654cf51506c09c7ae7e0a3e6ceca0d5
hunteac <135293451+hunteac@users.noreply.github.com>
2024-02-19 15:31:19 +0900

    [Gold V] Title: 감소하는 수, Time: 84 ms, Memory: 11644 KB -BaekjoonHub

--- 백준/Gold/1038. 감소하는 수/감소하는 수.java
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.java
@@ -1,72 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
+    static List<Long> list;
+
+    static void func(long num, int idx) {
+        if (idx > 10) return;
+
+        list.add(num);
+
+        for (int i = 0; i < num % 10; i++) {
+            func((num * 10) + i, idx + 1);
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        long num = 0;
-        int cnt = 0;
 
-        if (N == 0) {
-            System.out.println(0);
+        list = new ArrayList<>();
+
+        if (N <= 10) {
+            System.out.println(N);
             return;
-        } else if (N == 1022) {
-            System.out.println("9876543210");
-            return;
-        } else if (N > 1022) {
+        }
+
+        for (int i = 0; i < 10; i++) {
+            func(i, 1);
+        }
+
+        if (N >= list.size()) {
             System.out.println(-1);
             return;
         }
 
-        for (int i = 1; i <= 987654321; i++) {
-            String str = String.valueOf(i);
-            boolean chk = true;
-
-            if (str.charAt(0) - '0' < str.length() - 1) {
-                String next = "";
-                next += str.length() - 1;
-
-                for (int j = str.length() - 2; j >= 0; j--) {
-                    next += String.valueOf(j);
-                }
-
-                i = Integer.parseInt(next) - 1;
-                continue;
-            }
-
-            boolean chk2 = true;
-
-            for (int j = 0; j < str.length(); j++) {
-                if (j == 0) continue;
-                if (str.charAt(j - 1) <= str.charAt(j)) {
-                    chk = false;
-                } else if (str.charAt(j - 1) != str.charAt(j) - 1) {
-                    chk2 = false;
-                }
-            }
-
-            if (chk) cnt++;
-
-            if (chk2) {
-                String next = "";
-                next += str.charAt(0) - '0' + 1;
-
-                for (int j = str.length() - 2; j >= 0; j--) {
-                    next += String.valueOf(j);
-                }
-
-                i = Integer.parseInt(next) - 1;
-            }
-
-            if (cnt == N) {
-                num = Integer.parseInt(str);
-                break;
-            }
-        }
-
-        System.out.println(num);
+        Collections.sort(list);
+        System.out.println(list.get(N));
     }
 }
