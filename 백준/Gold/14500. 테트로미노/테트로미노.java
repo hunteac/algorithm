21d96fb63453201953d5296158e02169dbd1f6c1
hunteac <135293451+hunteac@users.noreply.github.com>
2024-02-26 14:45:46 +0900

    [Gold IV] Title: 테트로미노, Time: 500 ms, Memory: 31768 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -1,11 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+    static boolean[][] visited; // 방문 체크
     static int[][] map; // 종이
     static int[] dr = { -1, 0, 1, 0 }; // 행 탐색
     static int[] dc = { 0, 1, 0, -1 }; // 열 탐색
@@ -14,7 +13,7 @@ public class Main {
     static int max; // 최댓값
 
     // 깊이 우선 탐색을 활용한 테트로미노 만들기
-    static void dfs(int r, int c, int cnt, int score, List<Integer[]> check) {
+    static void dfs(int r, int c, int cnt, int score) {
         if (cnt == 1) { // 블록 두개 선택
             for (int di = 0; di < 3; di++) {
                 for (int dj = di + 1; dj < 4; dj++){
@@ -25,13 +24,15 @@ public class Main {
                     // 범위를 벗어나면 continue
                     if (fnr < 0 || fnr >= N || fnc < 0 || fnc >= M || snr < 0 || snr >= N || snc < 0 || snc >= M) continue;
 
-                    check.add(new Integer[]{r, c});
-                    check.add(new Integer[]{fnr, fnc});
-                    check.add(new Integer[]{snr, snc});
+                    visited[fnr][fnc] = true; // 방문 처리
+                    visited[snr][snc] = true;
 
-                    dfs(fnr, fnc, 3, score + map[fnr][fnc] + map[snr][snc], check);
-                    dfs(snr, snc, 3, score + map[fnr][fnc] + map[snr][snc], check);
-                    dfs(r, c, 3, score + map[fnr][fnc] + map[snr][snc], check);
+                    dfs(fnr, fnc, 3, score + map[fnr][fnc] + map[snr][snc]);
+                    dfs(snr, snc, 3, score + map[fnr][fnc] + map[snr][snc]);
+                    dfs(r, c, 3, score + map[fnr][fnc] + map[snr][snc]);
+
+                    visited[fnr][fnc] = false; // 백트래킹
+                    visited[snr][snc] = false;
                 }
             }
         } else if (cnt == 3) { // 나머지 블록 선택
@@ -39,10 +40,7 @@ public class Main {
                 int nr = r + dr[d]; // 다음 행
                 int nc = c + dc[d]; // 다음 열
                 // 범위를 벗어나거나 이미 선택한 블록은 continue
-                if (nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
-                for (int i = check.size() - 3; i < check.size(); i++) {
-                    if (check.get(i)[0] == nr && check.get(i)[1] == nc) continue Outer;
-                }
+                if (nr < 0 || nr >= N || nc < 0 || nc >= M || visited[nr][nc]) continue;
 
                 // 테트로미노 완성시 최댓값 갱신
                 if (score + map[nr][nc] > max) {
@@ -60,6 +58,7 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
         max = 0;
 
+        visited = new boolean[N][M];
         map = new int[N][M];
 
         for (int i = 0; i < N; i++) {
@@ -69,11 +68,10 @@ public class Main {
             }
         }
 
-        List<Integer[]> check = new ArrayList<>();
-
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                dfs(i, j, 1, map[i][j], check); // 너비 우선 탐색
+                visited[i][j] = true;
+                dfs(i, j, 1, map[i][j]); // 너비 우선 탐색
             }
         }
 
