ab52a56251284d2c2f92d68ce462e493a0067822
hunteac <135293451+hunteac@users.noreply.github.com>
2024-02-29 14:43:16 +0900

    [Gold IV] Title: Two Dots, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Gold/16929. Two Dots/Two Dots.java
+++ 백준/Gold/16929. Two Dots/Two Dots.java
@@ -6,28 +6,29 @@ import java.util.*;
 class Main {
     static boolean[][] visited; // 방문 체크
     static char[][] board; // 게임판
+    static int[][] boardCnt; // 사이클 순서
     static int[] dr = { -1, 0, 1, 0 }; // 행 탐색
     static int[] dc = { 0, 1, 0, -1 }; // 열 탐색
     static int N; // 행 크기
     static int M; // 열 크기
     static boolean chk;
 
-    static void dfs(int startR, int startC, int r, int c, int cnt, char color) {
+    static void dfs(int r, int c, int cnt, char color) {
         visited[r][c] = true; // 방문 처리
+        boardCnt[r][c] = cnt; // 순서 저장
 
         for (int d = 0; d < 4; d++) {
             int nr = r + dr[d];
             int nc = c + dc[d];
-            if (cnt > 2 && nr == startR && nc == startC && board[nr][nc] == color) { // 사이클 완성
+            if (nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
+            if (boardCnt[nr][nc] != 0 && cnt - boardCnt[nr][nc] > 2 && board[nr][nc] == color) { // 사이클 완성
                 chk = true;
                 return;
             }
-            if (nr < 0 || nr >= N || nc < 0 || nc >= M || visited[nr][nc] || board[nr][nc] != color) continue;
+            if (visited[nr][nc] || board[nr][nc] != color) continue;
 
-            dfs(startR, startC, nr, nc, cnt + 1, color);
+            dfs(nr, nc, cnt + 1, color);
         }
-
-        visited[r][c] = false; // 백트래킹
     }
 
     public static void main(String[] args) throws IOException {
@@ -39,6 +40,7 @@ class Main {
 
         visited = new boolean[N][M];
         board = new char[N][M];
+        boardCnt = new int[N][M];
 
         for (int i = 0; i < N; i++) {
             String str = br.readLine();
@@ -51,7 +53,8 @@ class Main {
 
         Outer: for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                dfs(i, j, i, j, 1, board[i][j]);
+                if (visited[i][j]) continue;
+                dfs(i, j, 1, board[i][j]);
                 if (chk) break Outer;
             }
         }
