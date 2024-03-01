70746cfab3c383039727ddc98ff381692a4aa612
hunteac <135293451+hunteac@users.noreply.github.com>
2024-03-01 22:00:01 +0900

    [Bronze V] Title: 세금, Time: 148 ms, Memory: 16072 KB -BaekjoonHub

---
+++ 백준/Bronze/20492. 세금/세금.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println((int) (N * 0.78) + " " + (int) (N - N * (0.2 * 0.22)));
+    }
+}
