61fff37b3e49e6e94d4943d516de00ea10a73708
hunteac <135293451+hunteac@users.noreply.github.com>
2024-03-07 12:44:51 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 148 ms, Memory: 14512 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -1,66 +1,59 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	static int N;
-	static int M;
-	static boolean[][] visited;
-	static boolean[][] edges;
-	static int[][] rel;
-	
-	static void dfs(int start, int r, int dist) {
-		if (rel[start][r] != 0) rel[start][r] = Math.min(rel[start][r], dist); 
-		else rel[start][r] = dist;
-		for (int c = 1; c <= N; c++) {
-			if ((start != r && edges[start][c]) || visited[r][c] || !edges[r][c]) continue;
-			visited[r][c] = visited[c][r] = true;
-			dfs(start, c, dist + 1);
-			visited[r][c] = visited[c][r] = false;
-		}
-	}
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		edges = new boolean[N + 1][N + 1];
-		rel = new int[N + 1][N + 1];
-		
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int A = Integer.parseInt(st.nextToken());
-			int B = Integer.parseInt(st.nextToken());
-			
-			edges[A][B] = true;
-			edges[B][A] = true;
-		}
-		
-		for (int i = 1; i <= N; i++) {
-			visited = new boolean[N + 1][N + 1];
-			dfs(i, i, 0);
-		}
-		
-		int min = Integer.MAX_VALUE;
-		int answer = 0;
-		
-		for (int i = 1; i <= N; i++) {
-			int sum = 0;
-			for (int j = 1; j <= N; j++) {
-				if (i == j) continue;
-				sum += rel[i][j];
-			}
-			
-			if (min > sum) {
-				min = sum;
-				answer = i;
-			}
-		}
-		
-		System.out.println(answer);
-	}
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] dist = new int[N + 1][N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i == j) continue;
+                dist[i][j] = 100; // 최대 노드 수 초기화
+            }
+        }
+
+        // 입력 받기
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            dist[A][B] = 1; // 양방향 연결
+            dist[B][A] = 1;
+        }
+
+        // 플로이드 와샬 알고리즘
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (i != j && dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+
+        int minSum = Integer.MAX_VALUE;
+        int num = 0;
+        for (int i = 1; i <= N; i++) {
+            int sum = 0;
+            for (int j = 1; j <= N; j++) {
+                sum += dist[i][j];
+            }
+            if (sum < minSum) {
+                minSum = sum;
+                num = i;
+            }
+        }
+
+        System.out.println(num);
+    }
 }
