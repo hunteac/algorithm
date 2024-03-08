b893da5c3da77e1b74da3e6e53f8ad644bb33e20
hunteac <135293451+hunteac@users.noreply.github.com>
2024-03-09 00:22:29 +0900

    [Silver I] Title: 가장 가까운 세 사람의 심리적 거리, Time: 196 ms, Memory: 18912 KB -BaekjoonHub

--- 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.java
+++ 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.java
@@ -1,9 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
+    static String[] MBTIs;
+    static int res = Integer.MAX_VALUE;
+
+    static int calcMBTIdist(int i, int j, int k) {
+        int cnt = 0;
+
+        for (int idx = 0; idx < 4; idx++) {
+            cnt += MBTIs[i].charAt(idx) != MBTIs[j].charAt(idx) ? 1 : 0;
+            cnt += MBTIs[j].charAt(idx) != MBTIs[k].charAt(idx) ? 1 : 0;
+            cnt += MBTIs[k].charAt(idx) != MBTIs[i].charAt(idx) ? 1 : 0;
+        }
+
+        return cnt;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -11,57 +26,34 @@ public class Main {
         int T = Integer.parseInt(br.readLine());
 
         for (int t = 0; t < T; t++) {
-            int N = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
 
-            HashMap<String, Integer> map1 = new HashMap<>(); // mbti별 등장 횟수
-            HashMap<Integer, String> map2 = new HashMap<>(); // mbti별 인덱스
+            MBTIs = new String[n];
+            res = Integer.MAX_VALUE;
 
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
 
-            int idx = 0;
-
-            for (int i = 0; i < N; i++) {
-                String mbti = st.nextToken();
-                map1.put(mbti, !map1.containsKey(mbti) ? 1 : map1.get(mbti) + 1); // 등장 횟수 구하기
-                if (!map2.containsValue(mbti)) map2.put(idx++, mbti);
+            if (n > 32) { // mbti가 동일한 세 명 존재
+                sb.append(0).append("\n");
+                continue;
             }
 
-            int min = Integer.MAX_VALUE; // 최소 거리
+            for (int i = 0; i < n; i++) {
+                MBTIs[i] = stringTokenizer.nextToken();
+            }
 
-            if (map1.size() == 1) { // 모두 같은 경우
-                min = 0;
-            } else if (map1.size() == 2) { // 한 명만 다른 경우
-                String A = map2.get(0);
-                String B = map2.get(1);
-                min = 0;
-
-                for (int i = 0; i < 4; i++) if (A.charAt(i) != B.charAt(i)) min += 2;
-            } else { // 완전 탐색
-                for (int i = 0; i < map2.size(); i++) {
-                    for (int j = 0; j < map2.size(); j++) {
-                        for (int k = 0; k < map2.size(); k++) {
-                            String A = map2.get(i);
-                            String B = map2.get(j);
-                            String C = map2.get(k);
-
-                            if (i == j && j == k && map1.get(A) < 3) continue;
-                            if ((i == j && map1.get(A) < 2) || (j == k && map1.get(B) < 2) || (i == k && map1.get(A) < 2)) continue;
-
-                            int dist = 0;
-
-                            for (int l = 0; l < 4; l++) {
-                                if (A.charAt(l) != B.charAt(l)) dist++;
-                                if (B.charAt(l) != C.charAt(l)) dist++;
-                                if (A.charAt(l) != C.charAt(l)) dist++;
-                            }
-
-                            min = Math.min(min, dist);
-                        }
+            for (int i = 0; i < n - 2; i++) {
+                if (res == 0) break;
+                for (int j = i + 1; j < n - 1; j++) {
+                    if (res == 0) break;
+                    for (int k = j + 1; k < n; k++) {
+                        res = Math.min(res, calcMBTIdist(i, j, k));
+                        if (res == 0) break;
                     }
                 }
             }
 
-            sb.append(min).append("\n");
+            sb.append(res).append("\n");
         }
 
         System.out.println(sb);
