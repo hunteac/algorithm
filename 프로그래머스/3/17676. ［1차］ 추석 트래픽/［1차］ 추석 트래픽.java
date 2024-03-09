26695db699e077bc2c83afa5253a51c356bbf93a
hunteac <135293451+hunteac@users.noreply.github.com>
2024-03-10 01:54:07 +0900

    [level 3] Title: [1차] 추석 트래픽, Time: ., Memory: undefined -BaekjoonHub

--- 프로그래머스/3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.java
+++ 프로그래머스/3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.java
@@ -1,45 +1,43 @@
-import java.util.*;
-
-class Traffic {
-    int start;
-    int end;
-    
-    Traffic(int start, int end){
-        this.start = start;
-        this.end = end;
-    }
-}
-
 class Solution {
     public int solution(String[] lines) {
         int answer = 0;
         
-        List<Traffic> t_list = new ArrayList();
+        int[] start = new int[lines.length];
+        int[] end = new int[lines.length];
         
-        for(int i = 0; i < lines.length; i++) {
-            // 초로 전환
-            int end =(int)((Integer.parseInt(lines[i].substring(11,13)) * 3600
-            + Integer.parseInt(lines[i].substring(14,16)) * 60) * 1000
-            + Double.parseDouble(lines[i].substring(17,23)) * 1000);
+        for (int i = 0; i < lines.length; i++) {
+            // 종료 시간 구하기
+            String tmp = lines[i].substring(11, 23);
+            tmp = tmp.replace(":", "");
+            tmp = tmp.replace(".", "");
             
-            int treat_time = (int)(Double.parseDouble(lines[i].substring(24, lines[i].length() - 1)) * 1000);
-            int start = end - treat_time + 1;
+            int endTime = Integer.parseInt(tmp); // 종료 시간 정수화
             
-            t_list.add(new Traffic(start, end));
-        }
-        
-        int cnt = 0;
-        
-        for(int i = 0; i < t_list.size(); i++) {
-            cnt = 1;
+            end[i] = endTime;
             
-            for(int j = i + 1; j < t_list.size(); j++){
-                if(t_list.get(i).end + 1000 > t_list.get(j).start) cnt++;
+            int idx = 25;
+            
+            // 시작 시간 구하기
+            tmp = String.valueOf(lines[i].charAt(24)); 
+            
+            while (lines[i].charAt(idx) != 's') {
+                if (lines[i].charAt(idx) != '.') tmp += lines[i].charAt(idx);
+                idx++;
             }
             
-            answer = Math.max(answer, cnt);
+            if (idx == 25) idx++;
+            
+            while (idx < 29) {
+                tmp += "0";
+                idx++;
+            }
+            
+            start[i] = endTime - Integer.parseInt(tmp) + 1;
+            
+            System.out.println("start : " + start[i]);
+            System.out.println("end : " + end[i]);
         }
-        
+          
         return answer;
     }
 }
