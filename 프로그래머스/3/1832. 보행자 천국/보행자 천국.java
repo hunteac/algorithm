fa92e78fc1277e06b27ec133b62cd227da2d4385
hunteac <135293451+hunteac@users.noreply.github.com>
2024-03-11 17:12:40 +0900

    [level 3] Title: 보행자 천국, Time: 282.15 ms, Memory: 108 MB -BaekjoonHub

---
+++ 프로그래머스/3/1832. 보행자 천국/보행자 천국.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+class Solution {
+    int MOD = 20170805;
+    public int solution(int m, int n, int[][] cityMap) {
+        int[][] cnt = new int[m][n]; // 경우의 수 누적합
+        
+        cnt[0][0] = 1; // 시작
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (cityMap[i][j] == 1) continue;
+                
+                if (i - 1 >= 0) {
+                    if (cityMap[i - 1][j] == 2) { // 아래 방향만 이동 가능
+                        int idx = i - 1;
+                        while (idx >= 0) {
+                            if (cityMap[idx][j] != 2) {
+                                cnt[i][j] = (cnt[i][j] + cnt[idx][j]) % MOD;
+                                break;
+                            }
+                            idx--;
+                        }
+                    } else {
+                        cnt[i][j] = (cnt[i][j] + cnt[i - 1][j]) % MOD;
+                    }
+                }
+                
+                if (j - 1 >= 0) {
+                    if (cityMap[i][j - 1] == 2) { // 오른쪽 방향만 이동 가능
+                        int idx = j - 1;
+                        while (idx >= 0) {
+                            if (cityMap[i][idx] != 2) {
+                                cnt[i][j] = (cnt[i][j] + cnt[i][idx]) % MOD;
+                                break;
+                            }
+                            idx--;
+                        }
+                    } else {
+                        cnt[i][j] = (cnt[i][j] + cnt[i][j - 1]) % MOD;    
+                    }
+                }
+            }
+        }
+        
+        // for (int i = 0; i < m; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         System.out.print(cnt[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+        
+        return cnt[m - 1][n - 1];
+    }
+}
