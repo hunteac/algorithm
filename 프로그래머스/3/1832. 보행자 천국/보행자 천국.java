a4b1ae10550a5bbf28d8d066872e5a1305ba57f2
hunteac <135293451+hunteac@users.noreply.github.com>
2024-03-11 17:12:59 +0900

    [level 3] Title: 보행자 천국, Time: 262.69 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/3/1832. 보행자 천국/보행자 천국.java
+++ 프로그래머스/3/1832. 보행자 천국/보행자 천국.java
@@ -43,13 +43,6 @@ class Solution {
             }
         }
         
-        // for (int i = 0; i < m; i++) {
-        //     for (int j = 0; j < n; j++) {
-        //         System.out.print(cnt[i][j] + " ");
-        //     }
-        //     System.out.println();
-        // }
-        
         return cnt[m - 1][n - 1];
     }
 }
