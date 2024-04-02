b2e2d60723c8d0a44f963c29b41b138bf2e48168
hunteac <135293451+hunteac@users.noreply.github.com>
2024-04-02 23:49:34 +0900

    [Bronze V] Title: CAPS, Time: 524 ms, Memory: 31308 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	 public static void main(String[] args) {
+	      Scanner scan = new Scanner(System.in);
+	      
+	      String s = scan.nextLine();
+	      s = s.toUpperCase();
+	    
+	      System.out.println(s);
+	 }
+}
