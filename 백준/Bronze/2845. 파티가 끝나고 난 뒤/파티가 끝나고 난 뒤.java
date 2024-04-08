bc1a7db042067acee2d1853dd3cf74cdfb664f52
hunteac <135293451+hunteac@users.noreply.github.com>
2024-04-08 23:28:01 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 140 ms, Memory: 15836 KB -BaekjoonHub

---
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        int l = Integer.parseInt(st.nextToken());
+        int p = Integer.parseInt(st.nextToken());
+        int num = l * p;
+        
+        st = new StringTokenizer(br.readLine());
+        
+        while (st.hasMoreTokens()) {
+            int n = Integer.parseInt(st.nextToken());
+            sb.append(n - num + " ");
+        }
+        
+        System.out.print(sb);
+    }
+}
