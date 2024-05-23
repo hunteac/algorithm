8b1046510da4003c515ad40e89b237139538111f
hunteac <135293451+hunteac@users.noreply.github.com>
2024-05-23 21:20:47 +0900

    [Bronze V] Title: Site Score, Time: 124 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+        
+		int ur = Integer.parseInt(st.nextToken());
+		int tr = Integer.parseInt(st.nextToken());
+		int uo = Integer.parseInt(st.nextToken());
+		int to = Integer.parseInt(st.nextToken());
+		
+		System.out.println(56*ur + 24*tr + 14*uo + 6*to);
+	}
+}
