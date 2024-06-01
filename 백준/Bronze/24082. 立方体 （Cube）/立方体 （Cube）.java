246fd8cc284369932c6700582f76e7a17b6755bf
hunteac <135293451+hunteac@users.noreply.github.com>
2024-06-01 23:42:44 +0900

    [Bronze V] Title: 立方体 (Cube), Time: 128 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/24082. 立方体 （Cube）/立方体 （Cube）.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        System.out.println(n * n * n);
+    }
+}
