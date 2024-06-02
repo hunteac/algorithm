b05c2285144434d136f39589adfec5ab979c4f97
hunteac <135293451+hunteac@users.noreply.github.com>
2024-06-02 20:48:30 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 120 ms, Memory: 14128 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        //어떤 수가 들어와도 값은 똑같기 때문에 입력받지 않아도 됨
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
