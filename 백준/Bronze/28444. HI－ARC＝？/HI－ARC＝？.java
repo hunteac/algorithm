140fa11644edb33bf983370ec0104ec1486f80ee
hunteac <135293451+hunteac@users.noreply.github.com>
2024-07-02 22:17:18 +0900

    [Bronze V] Title: HI-ARC=?, Time: 172 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/28444. HI－ARC＝？/HI－ARC＝？.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int H = sc.nextInt();
+        int I = sc.nextInt();
+        int A = sc.nextInt();
+        int R = sc.nextInt();
+        int C = sc.nextInt();
+        
+        System.out.print(H*I - A*R*C);
+    }
+}
