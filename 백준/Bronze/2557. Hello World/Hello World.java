a125023bf0142b830caa3c0dd71cc58b14d05560
hunteac <135293451+hunteac@users.noreply.github.com>
2024-07-13 17:32:29 +0900

    [Bronze V] Title: Hello World, Time: 88 ms, Memory: 13868 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -1,5 +1,8 @@
+import java.io.*;
+import java.util.*;
+
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
+    public static void main(String args[]) throws IOException {
+        System.out.println("Hello World!");
+    }
 }
