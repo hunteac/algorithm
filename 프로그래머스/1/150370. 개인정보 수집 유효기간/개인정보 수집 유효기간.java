cf6b256a9e3afe24b6051cb7a63202695729cdbd
hunteac <135293451+hunteac@users.noreply.github.com>
2025-05-15 22:12:31 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 32.37 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -2,45 +2,34 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-        int[] todayArr = Arrays.stream(today.split("\\.")).mapToInt(Integer::parseInt).toArray();
+        ArrayList<Integer> answer = new ArrayList<>();
         HashMap<String, Integer> termsMap = new HashMap<>();
         
         for (int i = 0; i < terms.length; i++) {
             String[] term = terms[i].split(" ");
-            String type = term[0];
-            String period = term[1];
             
-            termsMap.put(type, Integer.parseInt(period));
+            termsMap.put(term[0], Integer.parseInt(term[1]));
         }
-        
-        List<Integer> nums = new ArrayList<>();
         
         for (int i = 0; i < privacies.length; i++) {
             String[] privacy = privacies[i].split(" ");
-            String date = privacy[0];
-            String type = privacy[1];
             
-            int[] dateArr = Arrays.stream(date.split("\\.")).mapToInt(Integer::parseInt).toArray();
-            int period = termsMap.get(type);
+            int period = termsMap.get(privacy[1]);
             
-            dateArr[0] += (dateArr[1] + period - 1) / 12; 
-            dateArr[1] = (dateArr[1] + period) % 12 == 0 ? 12 : (dateArr[1] + period) % 12;
-            
-            if (todayArr[0] > dateArr[0]) {
-                nums.add(i + 1);
-            } else if (todayArr[0] == dateArr[0]) {
-                if (todayArr[1] > dateArr[1]) {
-                    nums.add(i + 1);
-                } else if (todayArr[1] == dateArr[1]) {
-                    if (todayArr[2] >= dateArr[2]) {
-                        nums.add(i + 1);
-                    }
-                }
+            if (getDate(today, 0) >= getDate(privacy[0], period)) {
+                answer.add(i + 1);
             }
         }
         
-        int[] answer = Arrays.stream(nums.toArray(new Integer[nums.size()])).mapToInt(Integer::intValue).toArray();;
+        return answer.stream().mapToInt(Integer -> Integer).toArray();
+    }
+    
+    public int getDate(String date, int period) {
+        int[] dateArr = Arrays.stream(date.split("\\.")).mapToInt(Integer::parseInt).toArray();
+        int year = dateArr[0];
+        int month = dateArr[1] + period;
+        int day = dateArr[2];
         
-        return answer;
+        return (year * 12 * 28) + (month * 28) + day;
     }
 }
