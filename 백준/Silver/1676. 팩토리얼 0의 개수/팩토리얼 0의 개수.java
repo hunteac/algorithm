3e266295169a50bdc96321a03e2dc46a474deb07
hunteac <135293451+hunteac@users.noreply.github.com>
2025-06-05 22:10:56 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 108 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -1,32 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        BigInteger num = new BigInteger("1");
+        long cnt;
+        long totalTwos;
+        long totalFives;
 
-        for (int i = 1; i <= N; i++) {
-            String s = String.valueOf(i);
-            BigInteger bs = new BigInteger(s);
+        totalTwos = countPrime(n, 2);
+        totalFives = countPrime(n, 5);
 
-            num = num.multiply(bs);
-        }
-
-        String snum = String.valueOf(num);
-        int cnt = 0;
-
-        // 뒤에서부터 0이 아닌 숫자가 나올 때까지 0의 개수 구하기
-        for (int i = snum.length() - 1; i >= 0; i--) {
-            if (snum.charAt(i) - '0' != 0) break;
-            cnt++;
-        }
+        cnt = Math.min(totalTwos, totalFives);
 
         System.out.println(cnt);
+    }
+
+    public static long countPrime(long n, int p) {
+        long cnt = 0;
+
+        for (long i = p; i <= n; i *= p) {
+            cnt += n / i;
+        }
+
+        return cnt;
     }
 }
