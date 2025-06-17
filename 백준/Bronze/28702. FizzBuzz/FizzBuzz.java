380c7a4f4635e1de13af38d2a4355ceb14ddf56c
hunteac <135293451+hunteac@users.noreply.github.com>
2025-06-17 17:52:41 +0900

    [Bronze I] Title: FizzBuzz, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/28702. FizzBuzz/FizzBuzz.java
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.java
@@ -6,31 +6,27 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        String A = br.readLine();
-        String B = br.readLine();
-        String C = br.readLine();
-        int max = 0, before = 0;
+        String[] strs = new String[3];
+
+        boolean flag = false;
+        int num = 0;
         
-        if (A.matches("[+-]?\\d*(\\.\\d+)?")) {
-        	max = Integer.parseInt(A);
-        	before = 3;
+        for (int i = 0; i < 3; i++) {
+        	strs[i] = br.readLine();
+        	
+        	if (strs[i].charAt(0) != 'F' && strs[i].charAt(0) != 'B') {
+        		num = Integer.parseInt(strs[i]);
+        		flag = true;
+        	}
+        	
+        	if (flag) {
+        		num++;
+        	}
         }
         
-        if (B.matches("[+-]?\\d*(\\.\\d+)?")) {
-        	max = Integer.parseInt(B);
-        	before = 2;
-        }
-        
-        if (C.matches("[+-]?\\d*(\\.\\d+)?")) {
-        	max = Integer.parseInt(C);
-        	before = 1;
-        }
-        
-        int next = max + before;
-        
-        if (next % 3 == 0 && next % 5 == 0) System.out.println("FizzBuzz");
-        else if (next % 3 == 0) System.out.println("Fizz");
-        else if (next % 5 == 0) System.out.println("Buzz");
-        else System.out.println(next);
+        if (num % 15 == 0) System.out.println("FizzBuzz");
+        else if (num % 3 == 0) System.out.println("Fizz");
+        else if (num % 5 == 0) System.out.println("Buzz");
+        else System.out.println(num);
     }
 }
