b84b7b8dc31a5964a7e720f42e223140c72024cc
hunteac <135293451+hunteac@users.noreply.github.com>
2025-06-17 10:41:00 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 288 ms, Memory: 47908 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -1,51 +1,42 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-
-    static char[][] stars;
-
-    static void recursion(int x, int y, int N, boolean blank) {
-        // 공백칸일 경우
-        if (blank) {
-            for (int i = x; i < x + N; i++) {
-                for (int j = y; j < y + N; j++) {
-                    stars[i][j] = ' ';
-                }
-            }
-            return;
-        }
-
-        // 더이상 쪼갤 수 없는 블럭
-        if (N == 1) {
-            stars[x][y] = '*';
-            return;
-        }
-
-        int size = N / 3;
-        int count = 0;
-        for (int i = x; i < x + N; i += size) {
-            for (int j = y; j < y + N; j += size) {
-                count++;
-                if (count == 5) recursion(i, j, size, true);
-                else recursion(i, j, size, false);
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+	public static char[][] star;
+	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int N = sc.nextInt();
-        stars = new char[N][N];
-
-        recursion(0, 0, N, false);
-
-        for (char[] star : stars) {
-            for (int i = 0; i < N; i++) {
-                sb.append(star[i]);
-            }
-            sb.append('\n');
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        star = new char[N][N];
+        
+        printStar(N, 0, 0);
+        
+        for (int i = 0; i < N; i++) {
+        	for (int j = 0; j < N; j++) {
+        		sb.append(star[i][j] == '*' ? '*' : ' ');
+        	}
+        	
+        	sb.append("\n");
         }
+        
         System.out.println(sb);
+    }
+    
+    public static void printStar(int N, int r, int c) {
+    	int next = N / 3;
+    	
+    	for (int i = 0; i < 3; i++) {
+    		for (int j = 0; j < 3; j++) {
+    			if (next > 0 && !(i == 1 && j == 1)) {
+    				printStar(next, r + (next * i), c + (next * j));
+    			}
+    		}
+    	}
+    	
+    	star[r][c] = '*';
     }
 }
