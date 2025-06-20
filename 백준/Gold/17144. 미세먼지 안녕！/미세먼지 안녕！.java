c247d972c7aa54e49a7896eae0c1c099c858ca28
hunteac <135293451+hunteac@users.noreply.github.com>
2025-06-20 15:27:51 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 332 ms, Memory: 39204 KB -BaekjoonHub

---
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
@@ -0,0 +1,109 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[][] room;
+	static int[] dr = {-1, 0, 1, 0};
+	static int[] dc = {0, 1, 0, -1};
+	static int R, C, T, r1, r2;
+	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        T = Integer.parseInt(st.nextToken());
+        
+        room = new int[R][C];
+        
+        for (int i = 0; i < R; i++) {
+        	st = new StringTokenizer(br.readLine());
+        	for (int j = 0; j < C; j++) {
+        		room[i][j] = Integer.parseInt(st.nextToken());
+        		if (room[i][j] == -1) {
+        			if (r1 == 0) r1 = i;
+        			else r2 = i;
+        		}
+        	}
+        }
+        
+        for (int i = 0; i < T; i++) {
+        	int[][] clone = new int[R][C];
+        	
+        	for (int r = 0; r < R; r++) {
+        		clone[r] = room[r].clone();
+        	}
+        	
+        	for (int r = 0; r < R; r++) {
+        		for (int c = 0; c < C; c++) {
+        			if (room[r][c] == -1 || room[r][c] == 0) continue;
+        			
+        			spreadDust(clone, r, c);
+        		}
+        	}
+        	
+        	cleanClockWise(r1, 1, -1); // 시계방향
+        	cleanAntiClockWise(r2, 1, -1); // 반시계방향
+        }
+        
+        int dust = 0;
+        
+        for (int i = 0; i < R; i++) {
+        	for (int j = 0; j < C; j++) {
+        		if (room[i][j] != -1) dust += room[i][j];
+        	}
+        }
+        
+        System.out.println(dust);
+    }
+    
+    public static void spreadDust(int[][] clone, int r, int c) {
+    	for (int d = 0; d < 4; d++) {
+			int nextR = r + dr[d];
+			int nextC = c + dc[d];
+			if (nextR < 0 || nextR >= R || nextC < 0 || nextC >= C || room[nextR][nextC] == -1) continue;
+			
+			room[r][c] -= clone[r][c] / 5;
+			room[nextR][nextC] += clone[r][c] / 5;
+			
+			room[r][c] = room[r][c] < 0 ? 0 : room[r][c];
+		}
+    }
+    
+    public static void cleanClockWise(int currR, int currC, int before) {
+    	if (room[currR][currC] == -1) return;
+    	
+    	int dust = room[currR][currC];
+    	
+		if (before != -1) room[currR][currC] = before;
+		else room[currR][currC] = 0;
+    	
+		if (currR == 0 && currC == 0) cleanClockWise(currR + 1, currC, dust);
+		else if (currR == 0 && currC == C - 1) cleanClockWise(currR, currC - 1, dust);
+		else if (currR == r1 && currC == C - 1) cleanClockWise(currR - 1, currC, dust);
+		else if (currR == 0) cleanClockWise(currR, currC - 1, dust);
+		else if (currR == r1) cleanClockWise(currR, currC + 1, dust);
+		else if (currC == 0) cleanClockWise(currR + 1, currC, dust);
+		else if (currC == C - 1) cleanClockWise(currR - 1, currC, dust);
+    }
+    
+    public static void cleanAntiClockWise(int currR, int currC, int before) {
+    	if (room[currR][currC] == -1) return;
+    	
+    	int dust = room[currR][currC];
+    	
+    	if (before != -1) room[currR][currC] = before;
+    	else room[currR][currC] = 0;
+    	
+		if (currR == r2 && currC == C - 1) cleanAntiClockWise(currR + 1, currC, dust);
+		else if (currR == R - 1 && currC == 0) cleanAntiClockWise(currR - 1, currC, dust);
+		else if (currR == R - 1 && currC == C - 1) cleanAntiClockWise(currR, currC - 1, dust);
+		else if (currR == r2) cleanAntiClockWise(currR, currC + 1, dust);
+		else if (currR == R - 1) cleanAntiClockWise(currR, currC - 1, dust);
+		else if (currC == 0) cleanAntiClockWise(currR - 1, currC, dust);
+		else if (currC == C - 1) cleanAntiClockWise(currR + 1, currC, dust);
+    }
+}
