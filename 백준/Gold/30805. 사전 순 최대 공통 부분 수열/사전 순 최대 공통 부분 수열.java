2c04c7d8b6aa94f3cf6706b9d50a316bafc9f6d3
hunteac <135293451+hunteac@users.noreply.github.com>
2025-06-24 16:20:20 +0900

    [Gold IV] Title: 사전 순 최대 공통 부분 수열, Time: 112 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Gold/30805. 사전 순 최대 공통 부분 수열/사전 순 최대 공통 부분 수열.java
+++ 백준/Gold/30805. 사전 순 최대 공통 부분 수열/사전 순 최대 공통 부분 수열.java
@@ -2,48 +2,80 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static HashMap<Integer, Integer> idxMap;
-	static ArrayList<Integer> result;
-	static int[] A, B;
-	static int N, M, max;
+	public static class Num implements Comparable<Num>{
+		int idx;
+		int value;
+		
+		Num(int idx, int value) {
+			this.idx = idx;
+			this.value = value;
+		}
+		
+		@Override
+		public int compareTo(Num num) {
+			return this.value == num.value ? this.idx - num.idx : num.value - this.value;
+		}
+	}
 	
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
         
-        N = Integer.parseInt(br.readLine());
-        A = new int[N];
+        PriorityQueue<Num> aQueue = new PriorityQueue<>();
+        PriorityQueue<Num> bQueue = new PriorityQueue<>();
+        
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
         
         st = new StringTokenizer(br.readLine());
         
-        for (int i = 0; i < N; i++) A[i] = Integer.parseInt(st.nextToken());
-        
-        M = Integer.parseInt(br.readLine());
-        B = new int[M];
-        
-        st = new StringTokenizer(br.readLine());
-        
-        for (int i = 0; i < M; i++) B[i] = Integer.parseInt(st.nextToken());
-        
-        idxMap = new HashMap<>();
-        result = new ArrayList<Integer>();
-
         for (int i = 0; i < N; i++) {
-        	int aNum = A[i];
-        	for (int j = 0; j < M; j++) {
-        		int bNum = B[j];
-        		boolean flag = false;
+        	A[i] = Integer.parseInt(st.nextToken());
+        	aQueue.add(new Num(i, A[i]));
+        }
+        
+        int M = Integer.parseInt(br.readLine());
+        int[] B = new int[M];
+        
+        st = new StringTokenizer(br.readLine());
+        
+        for (int i = 0; i < M; i++) {
+        	B[i] = Integer.parseInt(st.nextToken());
+        	bQueue.add(new Num(i, B[i]));
+        }
+        
+        ArrayList<Integer> result = new ArrayList<Integer>();
+
+        int lastAIdx = -1;
+        int lastBIdx = -1;
+        
+        while (!aQueue.isEmpty()) {
+        	Num aMax = aQueue.poll();
+        	int aIdx = aMax.idx;
+        	int aValue = aMax.value;
+        	
+        	if (lastAIdx > aIdx) continue;
+        	
+        	while (!bQueue.isEmpty()) {
+        		Num bMax = bQueue.peek();
+        		int bIdx = bMax.idx;
+            	int bValue = bMax.value;
         		
-        		if (aNum == bNum) {
-        			flag = getSequence(aNum, j);
-        		}
-        		
-        		if (flag) break;
+            	if (bValue < aValue) break;
+            	
+            	bQueue.poll();
+            	
+            	if (aValue == bValue && lastBIdx < bIdx) {
+            		result.add(aValue);
+            		lastAIdx = aIdx;
+            		lastBIdx = bIdx;
+            		break;
+            	}
         	}
         }
         
@@ -51,35 +83,5 @@ public class Main {
         for (Integer num : result) sb.append(num).append(" ");
 
         System.out.println(sb);
-    }
-    
-    public static boolean getSequence(int num, int idx) {
-    	if (num > max) { // 초기화
-			max = num;
-			result.removeAll(result);
-			idxMap.clear();
-		}
-		
-		int last = 0;
-		int lastIdx = 0;
-		
-		for (int k = 0; k < result.size(); k++) {
-			if (result.get(k) < num) break;
-			last = result.get(k);
-			lastIdx = k + 1;
-		}
-		
-		if (idxMap.containsKey(last) && idx <= idxMap.get(last)) {
-			return false;
-		} else {
-			while (lastIdx < result.size()) {
-				result.remove(lastIdx);
-			}
-			
-			result.add(num);
-			idxMap.put(num, idx);
-		}
-		
-		return true;
     }
 }
