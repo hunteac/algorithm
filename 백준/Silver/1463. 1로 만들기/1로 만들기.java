e47c0fe8785b8e55023a3a3ad06f3cbabe0f8d20
hunteac <135293451+hunteac@users.noreply.github.com>
2025-10-15 22:16:35 +0900

    [Silver III] Title: 1로 만들기, Time: 156 ms, Memory: 18148 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,37 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int X = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
-        int[] dp = new int[X + 1];
+        int[] cnts = new int[N + 1];
 
-        if (X == 1) {
-            System.out.println(0);
-            return;
-        } else if (X <= 3) {
-            System.out.println(1);
-            return;
+        Arrays.fill(cnts, Integer.MAX_VALUE);
+
+        cnts[1] = 0;
+
+        for (int i = 1; i <= N; i++) {
+            if (cnts[i] == Integer.MAX_VALUE) continue;
+
+            if (i + 1 <= N) {
+                cnts[i + 1] = Math.min(cnts[i] + 1, cnts[i + 1]);
+            }
+
+            if (i * 2 <= N) {
+                cnts[i * 2] = Math.min(cnts[i] + 1, cnts[i * 2]);
+            }
+
+            if (i * 3 <= N) {
+                cnts[i * 3] = Math.min(cnts[i] + 1, cnts[i * 3]);
+            }
         }
 
-        dp[2] = 1;
-        dp[3] = 1;
-
-        // 동적계획법
-        for (int i = 4; i <= X; i++) {
-            int min = Integer.MAX_VALUE;
-
-            if (i % 3 == 0) min = Math.min(min, dp[i / 3]);
-            if (i % 2 == 0) min = Math.min(min, dp[i / 2]);
-            min = Math.min(min, dp[i - 1]);
-
-            dp[i] = min + 1;
-        }
-
-        System.out.println(dp[X]);
+        System.out.println(cnts[N]);
     }
 }
