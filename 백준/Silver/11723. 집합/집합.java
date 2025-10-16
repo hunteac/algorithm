201790d43d316312253cf5b6a45b2b3b90234f42
hunteac <135293451+hunteac@users.noreply.github.com>
2025-10-16 22:28:04 +0900

    [Silver V] Title: 집합, Time: 1068 ms, Memory: 313636 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -2,55 +2,56 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
+    public static StringBuilder command;
 
-		Set<Integer> set = new HashSet<>();
-		
-		int M = Integer.parseInt(br.readLine());
-		int num = 0;
-		
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			String command = st.nextToken();
-			
-			switch (command) {
-				case "add":
-					num = Integer.parseInt(st.nextToken());
-					set.add(num);
-					break;
-				case "remove":
-					num = Integer.parseInt(st.nextToken());
-					if (set.contains(num)) set.remove(num);
-					break;
-				case "check":
-					num = Integer.parseInt(st.nextToken());
-					if (set.contains(num)) sb.append(1).append("\n");
-					else sb.append(0).append("\n");
-					break;
-				case "toggle":
-					num = Integer.parseInt(st.nextToken());
-					if (set.contains(num)) set.remove(num); 
-					else set.add(num);
-					break;
-				case "all":
-					for (int j = 1; j <= 20; j++) {
-						if (set.contains(j)) continue;
-						set.add(j);
-					}
-					break;
-				case "empty":
-					set.clear();
-					break;
-			}
-		}
-		
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int M = Integer.parseInt(br.readLine());
+
+        HashSet<Integer> set = new HashSet<>();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            String com = st.nextToken();
+
+            if ("all".equals(com)) {
+                for (int j = 1; j <= 20; j++) {
+                    set.add(j);
+                }
+                continue;
+            } else if ("empty".equals(com)) {
+                set.clear();
+                continue;
+            }
+
+            int num = Integer.parseInt(st.nextToken());
+
+            if ("add".equals(com)) {
+                set.add(num);
+            } else if ("remove".equals(com)) {
+                set.remove(num);
+            } else if ("check".equals(com)) {
+                if (set.contains(num)) {
+                    sb.append(1).append("\n");
+                } else {
+                    sb.append(0).append("\n");
+                }
+            } else if ("toggle".equals(com)) {
+                if (set.contains(num)) {
+                    set.remove(num);
+                } else {
+                    set.add(num);
+                }
+            }
+        }
+
+        System.out.println(sb);
+    }
 }
