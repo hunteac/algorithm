8a56ca0562050c180d96848b31110f9a8cf76b10
hunteac <135293451+hunteac@users.noreply.github.com>
2025-10-16 21:51:14 +0900

    [Bronze I] Title: 디지털 티비, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/2816. 디지털 티비/디지털 티비.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static StringBuilder command;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        command = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        String[] channels = new String[N];
+
+        int idx1 = 0;
+        int idx2 = 0;
+
+        for (int i = 0; i < N; i++) {
+            channels[i] = br.readLine();
+            if ("KBS1".equals(channels[i])) {
+                idx1 = i;
+            } else if ("KBS2".equals(channels[i])) {
+                idx2 = i;
+            }
+        }
+
+        if (idx1 < idx2) idx1++;
+
+        getCommand("1", idx2);
+        getCommand("4", idx2);
+        getCommand("1", idx1);
+        getCommand("4", idx1);
+
+        System.out.println(command);
+    }
+
+    public static void getCommand(String com, int idx) {
+        for (int i = 0; i < idx; i++) {
+            command.append(com);
+        }
+    }
+}
