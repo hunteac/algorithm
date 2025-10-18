da562b267b637748611932c1699f9a2eb921467f
hunteac <135293451+hunteac@users.noreply.github.com>
2025-10-18 12:07:10 +0900

    [Gold IV] Title: 최단경로, Time: 692 ms, Memory: 109196 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,90 +1,73 @@
-import javax.xml.soap.Node;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static class Node {
-        int v; // 다음 노드
-        int w; // 가중치
+    public static class Node implements Comparable<Node> {
+        int next;
+        int dist;
 
-        Node (int v, int w) {
-            this.v = v;
-            this.w = w;
+        Node(int next, int dist) {
+            this.next = next;
+            this.dist = dist;
+        }
+
+        public int compareTo(Node o) {
+            return this.dist - o.dist;
         }
     }
 
-    static final int INF = 987654321;
-    static PriorityQueue<Node> queue; // 노드 우선순위 큐
-    static List<Node>[] adjList; // 인접리스트
-    static int[] dist; // 노드별 최단경로
-    static int V; // 정점
-    static int E; // 간선
-    static int start; // 시작 정점
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        V = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
-        start = Integer.parseInt(br.readLine());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(br.readLine());
 
-        // 가중치를 기준으로 오름차순 정렬
-        queue = new PriorityQueue<>(new Comparator<Node>() {
-           @Override
-           public int compare (Node o1, Node o2) {
-               return o1.w - o2.w;
-           }
-        });
+        int[] dists = new int[V + 1];
+        Arrays.fill(dists, Integer.MAX_VALUE);
 
-        adjList = new ArrayList[V + 1];
-        dist = new int[V + 1];
+        List<List<Node>> edges = new ArrayList<>();
 
-        for (int i = 1; i <= V; i++) {
-            adjList[i] = new ArrayList<>();
+        for (int i = 0; i <= V; i++) {
+            edges.add(new ArrayList<>());
         }
-
-        Arrays.fill(dist, INF);
 
         for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
+
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
 
-            adjList[u].add(new Node(v, w));
+            edges.get(u).add(new Node(v, w));
         }
 
-        dijkstra();
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(K, 0));
 
-        for (int i = 1; i <= V; i++) {
-            if (dist[i] == INF) System.out.println("INF");
-            else System.out.println(dist[i]);
-        }
-    }
-    // 다익스트라 알고리즘으로 최단 경로 구하기
-    static void dijkstra() {
-        boolean[] visited = new boolean[V + 1];
-        queue.add(new Node(start, 0)); // 시작 정점
-        dist[start] = 0; // 시작 정점의 경로는 0
+        while (!pq.isEmpty()) {
+            Node node = pq.poll();
 
-        while (!queue.isEmpty()) {
-            Node curr = queue.poll(); // 현재 정점
-            int v = curr.v;
-
-            if (visited[v]) continue; // 방문 체크
-            visited[v] = true;
-
-            for (int i = 0; i < adjList[v].size(); i++) {
-                Node next = adjList[v].get(i); // 연결된 다음 정점
-
-                if (dist[next.v] > dist[v] + next.w) { // 간선의 가중치가 더 작다면 값 갱신
-                    dist[next.v] = dist[v] + next.w;
-                    queue.add(new Node(next.v, dist[next.v]));
+            for (Node nextNode : edges.get(node.next)) {
+                if (dists[nextNode.next] > node.dist + nextNode.dist) {
+                    dists[nextNode.next] = node.dist + nextNode.dist;
+                    pq.add(new Node(nextNode.next, dists[nextNode.next]));
                 }
             }
         }
+
+        for (int i = 1; i <= V; i++) {
+            if (i == K) sb.append(0);
+            else if (dists[i] != Integer.MAX_VALUE) sb.append(dists[i]);
+            else sb.append("INF");
+
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
     }
 }
