e8fd3653c8eaf18023609ea7a714ebd7af72bf3d
hunteac <135293451+hunteac@users.noreply.github.com>
2026-03-23 22:26:33 +0900

    [Silver V] Title: 뒤집기, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.java
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -1,27 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String word = br.readLine();
+        String str = br.readLine();
 
+        char before = ' ';
         int zeroCnt = 0;
         int oneCnt = 0;
 
-        char before = ' ';
+        for (int i = 0; i < str.length(); i++) {
+            char curr = str.charAt(i);
 
-        for (int i = 0; i < word.length(); i++) {
-            char curr = word.charAt(i);
-
-            if (curr != before) {
+            if (before != curr) {
                 if (curr == '0') {
-                    zeroCnt++;
-                } else {
                     oneCnt++;
+                } else {
+                    zeroCnt++;
                 }
             }
 
