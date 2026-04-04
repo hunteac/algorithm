6a28778c46ca939d81ddbc5871d171a24fcd18f4
hunteac <135293451+hunteac@users.noreply.github.com>
2026-04-04 17:06:39 +0900

    [Gold IV] Title: 연구소, Time: 500 ms, Memory: 154120 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -4,16 +4,6 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static class Virus {
-        int r;
-        int c;
-
-        Virus(int r, int c) {
-            this.r = r;
-            this.c = c;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -22,76 +12,78 @@ public class Main {
         int M = Integer.parseInt(st.nextToken());
 
         int[][] map = new int[N][M];
-        List<Virus> viruses = new ArrayList<>();
-        int[] dr = {-1, 0, 1, 0};
-        int[] dc = {0, 1, 0, -1};
+
+        ArrayList<Integer[]> virusList = new ArrayList<>();
+        ArrayList<Integer[]> emptyList = new ArrayList<>();
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                if (map[i][j] == 2) viruses.add(new Virus(i, j));
+                if (map[i][j] == 0) {
+                    emptyList.add(new Integer[]{i, j});
+                } else if (map[i][j] == 2) {
+                    virusList.add(new Integer[]{i, j});
+                }
             }
         }
 
+        int[] dr = {-1, 0, 1, 0};
+        int[] dc = {0, 1, 0, -1};
+
         int max = 0;
 
-        Queue<Virus> queue;
-
-        for (int i = 0; i < N * M; i++) {
-            for (int j = i + 1; j < N * M; j++) {
-                for (int k = j + 1; k < N * M; k++) {
-                    int firstR = i / M;
-                    int firstC = i % M;
-                    int secondR = j / M;
-                    int secondC = j % M;
-                    int thirdR = k / M;
-                    int thirdC = k % M;
-
-                    if (map[firstR][firstC] != 0 || map[secondR][secondC] != 0 || map[thirdR][thirdC] != 0) {
-                        continue;
-                    }
-
+        for (int i = 0; i < emptyList.size() - 2; i++) {
+            for (int j = i + 1; j < emptyList.size() - 1; j++) {
+                for (int k = j + 1; k < emptyList.size(); k++) {
+                    // 새로운 지도 만들기
                     int[][] newMap = new int[N][M];
-                    int cnt = 0;
 
-                    for (int r = 0; r < N; r++) {
-                        newMap[r] = map[r].clone();
+                    for (int idx = 0; idx < N; idx++) {
+                        newMap[idx] = map[idx].clone();
                     }
 
-                    newMap[firstR][firstC] = newMap[secondR][secondC] = newMap[thirdR][thirdC] = 1;
+                    // 벽 세우기
+                    newMap[emptyList.get(i)[0]][emptyList.get(i)[1]] = 1;
+                    newMap[emptyList.get(j)[0]][emptyList.get(j)[1]] = 1;
+                    newMap[emptyList.get(k)[0]][emptyList.get(k)[1]] = 1;
 
-                    queue = new LinkedList<>();
+                    // 바이러스 퍼트리기
+                    Queue<Integer[]> queue = new LinkedList<>();
 
-                    for (Virus v : viruses) {
-                        queue.add(new Virus(v.r, v.c));
+                    for (Integer[] virus : virusList) {
+                        queue.add(new Integer[]{virus[0], virus[1]});
                     }
 
                     while (!queue.isEmpty()) {
-                        Virus v = queue.poll();
-
-                        int r = v.r;
-                        int c = v.c;
+                        Integer[] virus = queue.poll();
+                        int r = virus[0];
+                        int c = virus[1];
 
                         for (int d = 0; d < 4; d++) {
                             int nr = r + dr[d];
                             int nc = c + dc[d];
-                            if (nr < 0 || nr >= N || nc < 0 || nc >= M || newMap[nr][nc] != 0) {
-                                continue;
-                            }
 
-                            newMap[nr][nc] = 2;
-                            queue.add(new Virus(nr, nc));
+                            if (nr >= 0 && nr < N && nc >= 0 && nc < M) {
+                                if (newMap[nr][nc] == 0) {
+                                    newMap[nr][nc] = 2;
+                                    queue.add(new Integer[]{nr, nc});
+                                }
+                            }
                         }
                     }
 
+                    // 안전영역 계산
+                    int tmpMax = 0;
                     for (int r = 0; r < N; r++) {
                         for (int c = 0; c < M; c++) {
-                            if (newMap[r][c] == 0) cnt++;
+                            if (newMap[r][c] == 0) {
+                                tmpMax++;
+                            }
                         }
                     }
 
-                    max = Math.max(max, cnt);
+                    max = Math.max(max, tmpMax);
                 }
             }
         }
