a6ce9bce8447281662c6e9fcb9c03e3a5698d10c
hunteac <135293451+hunteac@users.noreply.github.com>
2026-04-04 16:07:59 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 872 ms, Memory: 260176 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -2,64 +2,66 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    static ArrayList<ArrayList<Integer>> nodeList;
+    static int[] dist;
 
-    public static List<List<Integer>> road;
-    public static boolean[][] visited;
-    public static int[] dists;
-    public static int N, M, K, X;
+    public static void dfs(int curr, int d, int max) {
+        for (int i = 0; i < nodeList.get(curr).size(); i++) {
+            int next = nodeList.get(curr).get(i);
+            if (dist[next] > d + 1) {
+                dist[next] = d + 1;
+                if (d + 1 < max) {
+                    dfs(next, d + 1, max);
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        X = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
 
-        road = new ArrayList<>();
-        dists = new int[N + 1];
+        nodeList = new ArrayList<>();
 
         for (int i = 0; i <= N; i++) {
-            road.add(new ArrayList<>());
-            dists[i] = Integer.MAX_VALUE;
+            nodeList.add(new ArrayList<>());
         }
-
-        dists[X] = 0;
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
-
-            road.get(A).add(B);
+            nodeList.get(A).add(B);
         }
 
-        dfs(X, 1);
+        dist = new int[N + 1];
+
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[X] = 0;
+
+        dfs(X, 0, K);
+
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 1; i <= N; i++) {
-            if (dists[i] == K) {
+            if (dist[i] == K) {
                 sb.append(i).append("\n");
             }
         }
 
-        if (sb.length() != 0) System.out.println(sb);
-        else System.out.println(-1);
-    }
-
-    public static void dfs(int curr, int dist) {
-        for (int i = 0; i < road.get(curr).size(); i++) {
-            int next = road.get(curr).get(i);
-            if (dists[next] > dist) {
-                dists[next] = dist;
-                if (dist < K) dfs(next, dist + 1);
-            }
+        if (sb.length() != 0) {
+            System.out.println(sb);
+        } else {
+            System.out.println(-1);
         }
     }
 }
