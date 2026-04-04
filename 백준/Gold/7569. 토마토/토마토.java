54e96911b75e84a8d148f7fa2a31e23d0e0e8093
hunteac <135293451+hunteac@users.noreply.github.com>
2026-04-04 20:33:39 +0900

    [Gold V] Title: 토마토, Time: 680 ms, Memory: 128824 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -1,113 +1,87 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	static class Pos {
-		int h;
-		int r;
-		int c;
-		
-		Pos (int h, int r, int c){
-			this.h = h;
-			this.r = r;
-			this.c = c;
-		}
-	}
-	
-	static Queue<Pos> queue;
-	static int N; // 세로
-	static int M; // 가로
-	static int H; // 높이
-	static boolean[][][] visited; // 방문 체크
-	static int[][][] map; // 토마토 밭
-	static int[] dr = { 0, 0, -1, 0, 1, 0 }; // 행 탐색
-	static int[] dc = { 0, 0, 0, 1, 0, -1 }; // 열 탐색
-	static int[] dh = { 1, -1, 0, 0, 0, 0 }; // 높이 탐색
-	static int min; // 최소 시간
-	
-	static void bfs() {
-		while (!queue.isEmpty()) {
-			int len = queue.size();
-			
-			for (int i = 0; i < len; i++) {
-				Pos p = queue.poll();
-				
-				int h = p.h;
-				int r = p.r;
-				int c = p.c;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-				for (int d = 0; d < 6; d++) {
-					int nh = h + dh[d];
-					int nr = r + dr[d];
-					int nc = c + dc[d];
-					
-					if (nh < 0 || nr < 0 || nc < 0 || nh >= H || nr >= M || nc >= N || 
-							visited[nh][nr][nc] || map[nh][nr][nc] == -1)
-						continue;
-					
-					visited[nh][nr][nc] = true;
-					map[nh][nr][nc] = 1;
-					
-					queue.add(new Pos(nh, nr, nc));
-				}
-			}
-			
-			min++;
-		}
-	}
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken()); 
-		M = Integer.parseInt(st.nextToken()); 
-		H = Integer.parseInt(st.nextToken()); 
-		
-		queue = new LinkedList<>();
-		visited = new boolean[H][M][N];
-		map = new int[H][M][N];
-		
-		boolean chk = true;
-		
-		for (int h = 0; h < H; h++) {
-			for (int m = 0; m < M; m++) {
-				st = new StringTokenizer(br.readLine());
-				for (int n = 0; n < N; n++) {
-					map[h][m][n] = Integer.parseInt(st.nextToken());
-					if (map[h][m][n] == 1) {
-						queue.add(new Pos(h, m, n));
-						visited[h][m][n] = true;
-					}
-					else if (map[h][m][n] == 0) chk = false;
-				}
-			}
-		}
-		
-		if (chk) {
-			System.out.println(0);
-			return;
-		}
-		
-		min = -1;
-		
-		bfs();
-		
-		for (int h = 0; h < H; h++) {
-			for (int m = 0; m < M; m++) {
-				for (int n = 0; n < N; n++) {
-					if (map[h][m][n] == 0) {
-						System.out.println(-1);
-						return;
-					}
-				}
-			}
-		}
-		
-		System.out.println(min);
-	}
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int H = Integer.parseInt(st.nextToken());
+
+        int[][][] map = new int[H][N][M];
+
+        Queue<Integer[]> tomatoes = new LinkedList<>();
+
+        int cnt = 0;
+
+        for (int h = 0; h < H; h++) {
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < M; j++) {
+                    map[h][i][j] = Integer.parseInt(st.nextToken());
+                    if (map[h][i][j] == 1) {
+                        tomatoes.add(new Integer[]{h, i, j});
+                    } else if (map[h][i][j] == 0) {
+                        cnt++;
+                    }
+                }
+            }
+        }
+
+        if (cnt == 0) {
+            System.out.println(0);
+            return;
+        }
+
+        int[] dh = {-1, 1, 0, 0, 0, 0};
+        int[] dr = {0, 0, -1, 0, 1, 0};
+        int[] dc = {0, 0, 0, 1, 0, -1};
+
+        int days = 0;
+
+        while (!tomatoes.isEmpty()) {
+            int len = tomatoes.size();
+
+            for (int i = 0; i < len; i++) {
+                Integer[] tomato = tomatoes.poll();
+                int h = tomato[0];
+                int r = tomato[1];
+                int c = tomato[2];
+
+                for (int d = 0; d < 6; d++) {
+                    int nh = h + dh[d];
+                    int nr = r + dr[d];
+                    int nc = c + dc[d];
+
+                    if (nh >= 0 && nh < H && nr >= 0 && nr < N && nc >= 0 && nc < M) {
+                        if (map[nh][nr][nc] == 0) {
+                            map[nh][nr][nc] = 1;
+                            tomatoes.add(new Integer[]{nh, nr, nc});
+                        }
+                    }
+                }
+            }
+
+            if (!tomatoes.isEmpty()) {
+                days++;
+            }
+        }
+
+        for (int h = 0; h < H; h++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (map[h][i][j] == 0) {
+                        System.out.println(-1);
+                        return;
+                    }
+                }
+            }
+        }
+
+        System.out.println(days);
+    }
 }
