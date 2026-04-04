38e093b07ed78ac8af5c3f6e71cfbcd77b06ff6c
hunteac <135293451+hunteac@users.noreply.github.com>
2026-04-04 18:15:18 +0900

    [Gold IV] Title: 최단경로, Time: 716 ms, Memory: 112200 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -5,67 +5,73 @@ import java.util.*;
 
 public class Main {
     public static class Node implements Comparable<Node> {
-        int next;
+        int num;
         int dist;
 
-        Node(int next, int dist) {
-            this.next = next;
+        Node(int num, int dist) {
+            this.num = num;
             this.dist = dist;
         }
 
+        @Override
         public int compareTo(Node o) {
-            return this.dist - o.dist;
+            return Integer.compare(this.dist, o.dist);
         }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int V = Integer.parseInt(st.nextToken());
         int E = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(br.readLine());
 
-        int[] dists = new int[V + 1];
-        Arrays.fill(dists, Integer.MAX_VALUE);
+        int[] dist = new int[V + 1];
 
-        List<List<Node>> edges = new ArrayList<>();
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[K] = 0;
+
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        ArrayList<Integer[]>[] list = new ArrayList[V + 1];
 
         for (int i = 0; i <= V; i++) {
-            edges.add(new ArrayList<>());
+            list[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
-
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
-
-            edges.get(u).add(new Node(v, w));
+            list[u].add(new Integer[]{v, w});
         }
 
-        PriorityQueue<Node> pq = new PriorityQueue<>();
         pq.add(new Node(K, 0));
 
-        while (!pq.isEmpty()) {
-            Node node = pq.poll();
+        while(!pq.isEmpty()) {
+            Node n = pq.poll();
+            int num = n.num;
+            int d = n.dist;
 
-            for (Node nextNode : edges.get(node.next)) {
-                if (dists[nextNode.next] > node.dist + nextNode.dist) {
-                    dists[nextNode.next] = node.dist + nextNode.dist;
-                    pq.add(new Node(nextNode.next, dists[nextNode.next]));
+            for (int i = 0; i < list[num].size(); i++) {
+                int next = list[num].get(i)[0];
+                int weight = list[num].get(i)[1];
+                if (dist[next] > d + weight) {
+                    dist[next] = d + weight;
+                    pq.add(new Node(next, dist[next]));
                 }
             }
         }
 
-        for (int i = 1; i <= V; i++) {
-            if (i == K) sb.append(0);
-            else if (dists[i] != Integer.MAX_VALUE) sb.append(dists[i]);
-            else sb.append("INF");
+        StringBuilder sb = new StringBuilder();
 
-            sb.append("\n");
+        for (int i = 1; i <= V; i++) {
+            if (dist[i] != Integer.MAX_VALUE) {
+                sb.append(dist[i]).append("\n");
+            } else {
+                sb.append("INF").append("\n");
+            }
         }
 
         System.out.println(sb);
